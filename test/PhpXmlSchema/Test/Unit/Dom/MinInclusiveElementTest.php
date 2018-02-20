a950193cdbffe5e377968daec3abb4200b6604b7
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 05:24:53 +0100

    Create the "minInclusive" element.

---
+++ test/PhpXmlSchema/Test/Unit/Dom/MinInclusiveElementTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PhpXmlSchema\Dom\MinInclusiveElement;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\MinInclusiveElement} 
+ * class.
+ * 
+ * @group   element
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class MinInclusiveElementTest extends AbstractCompositeElementTestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->sut = new MinInclusiveElement();
+    }
+}
