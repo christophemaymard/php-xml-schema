c3924334c9e9ea51a30fc48223e68379b128e808
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 05:33:44 +0100

    Create the "selector" element.

---
+++ test/PhpXmlSchema/Test/Unit/Dom/SelectorElementTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PhpXmlSchema\Dom\SelectorElement;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\SelectorElement} 
+ * class.
+ * 
+ * @group   element
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class SelectorElementTest extends AbstractCompositeElementTestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->sut = new SelectorElement();
+    }
+}
