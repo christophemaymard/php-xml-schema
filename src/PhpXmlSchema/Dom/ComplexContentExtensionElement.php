0f11034a8272a44f04f3a6c68a5145e99cde1bc0
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 04:45:39 +0100

    Create the "extension" element (complexContent).

---
+++ src/PhpXmlSchema/Dom/ComplexContentExtensionElement.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Represents the XML schema "extension" element held in the XML schema 
+ * "complexContent" element ({@see PhpXmlSchema\Dom\ComplexContentElement}).
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ComplexContentExtensionElement extends AbstractCompositeElement
+{
+}
---
+++ test/PhpXmlSchema/Test/Unit/Dom/ComplexContentExtensionElementTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PhpXmlSchema\Dom\ComplexContentExtensionElement;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\ComplexContentExtensionElement} 
+ * class.
+ * 
+ * @group   element
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ComplexContentExtensionElementTest extends AbstractCompositeElementTestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->sut = new ComplexContentExtensionElement();
+    }
+}
