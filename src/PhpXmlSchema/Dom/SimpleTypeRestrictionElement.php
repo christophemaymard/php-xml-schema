8e98d8eafb74d4ae24347bf8bbbac0799356af61
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 05:49:11 +0100

    Create the "restriction" element (simpleType).

---
+++ src/PhpXmlSchema/Dom/SimpleTypeRestrictionElement.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Represents the XML schema "restriction" element held in the XML schema 
+ * "simpleType" element ({@see PhpXmlSchema\Dom\SimpleTypeElement}).
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class SimpleTypeRestrictionElement extends AbstractCompositeElement
+{
+}
