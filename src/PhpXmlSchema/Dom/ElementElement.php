780a6edd733ee5cf15dc6e1d6b3a788867ad1327
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 21:50:30 +0100

    Manage identity-constraint elements.

--- src/PhpXmlSchema/Dom/ElementElement.php
+++ src/PhpXmlSchema/Dom/ElementElement.php
@@ -46,4 +46,74 @@ class ElementElement extends AbstractAnnotatedElement
     {
         return $this->isChildElementSet(1);
     }
+    
+    /**
+     * Adds an "unique" element to this element.
+     * 
+     * @param   UniqueElement   $element    The element to add.
+     */
+    public function addUniqueElement(UniqueElement $element)
+    {
+        $this->addChildElement(2, $element);
+    }
+    
+    /**
+     * Returns all the "unique" child elements.
+     * 
+     * @return  UniqueElement[] An indexed array of UniqueElement instances.
+     */
+    public function getUniqueElements():array
+    {
+        return $this->getChildElementsByType(2, UniqueElement::class);
+    }
+    
+    /**
+     * Adds a "key" element to this element.
+     * 
+     * @param   KeyElement  $element    The element to add.
+     */
+    public function addKeyElement(KeyElement $element)
+    {
+        $this->addChildElement(2, $element);
+    }
+    
+    /**
+     * Returns all the "key" child elements.
+     * 
+     * @return  KeyElement[]    An indexed array of KeyElement instances.
+     */
+    public function getKeyElements():array
+    {
+        return $this->getChildElementsByType(2, KeyElement::class);
+    }
+    
+    /**
+     * Adds a "keyref" element to this element.
+     * 
+     * @param   KeyRefElement   $element    The element to add.
+     */
+    public function addKeyRefElement(KeyRefElement $element)
+    {
+        $this->addChildElement(2, $element);
+    }
+    
+    /**
+     * Returns all the "keyref" child elements.
+     * 
+     * @return  KeyRefElement[] An indexed array of KeyRefElement instances.
+     */
+    public function getKeyRefElements():array
+    {
+        return $this->getChildElementsByType(2, KeyRefElement::class);
+    }
+    
+    /**
+     * Returns all the identity-constraint child elements.
+     * 
+     * @return  IdentityConstraintElementInterface[]    An indexed array of IdentityConstraintElementInterface instances.
+     */
+    public function getIdentityConstraintElements():array
+    {
+        return $this->getChildElementsByType(2, IdentityConstraintElementInterface::class);
+    }
 }
--- test/PhpXmlSchema/Test/Unit/Dom/ElementElementTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/ElementElementTest.php
@@ -76,4 +76,146 @@ class ElementElementTest extends AbstractAnnotatedElementTestCase
         $this->sut->setTypeElement($children[0]);
         self::assertSame($children, $this->sut->getElements(), 'Elements in container 1.');
     }
+    
+    /**
+     * Tests that getUniqueElements() returns:
+     * - an empty array when no element has been added
+     * - an empty array when no UniqueElement element has been added
+     * - an indexed array of all added UniqueElement elements
+     * 
+     * @group   elt-content
+     */
+    public function testGetUniqueElements()
+    {
+        $elements = [];
+        
+        self::assertSame($elements, $this->sut->getUniqueElements(), 'No element has been added.');
+        
+        $this->sut->addKeyElement($this->createKeyElementDummy());
+        $this->sut->addKeyRefElement($this->createKeyRefElementDummy());
+        self::assertSame($elements, $this->sut->getUniqueElements(), 'Added elements but no UniqueElement element.');
+        
+        $elements[] = $this->createUniqueElementDummy();
+        $this->sut->addUniqueElement($elements[0]);
+        self::assertSame($elements, $this->sut->getUniqueElements(), 'Added 1 UniqueElement element.');
+        
+        $this->sut->addKeyElement($this->createKeyElementDummy());
+        self::assertSame($elements, $this->sut->getUniqueElements(), 'Added 1 element between.');
+        
+        $elements[] = $this->createUniqueElementDummy();
+        $this->sut->addUniqueElement($elements[1]);
+        self::assertSame($elements, $this->sut->getUniqueElements(), 'Added 2 UniqueElement elements.');
+    }
+    
+    /**
+     * Tests that getKeyElements() returns:
+     * - an empty array when no element has been added
+     * - an empty array when no KeyElement element has been added
+     * - an indexed array of all added KeyElement elements
+     * 
+     * @group   elt-content
+     */
+    public function testGetKeyElements()
+    {
+        $elements = [];
+        
+        self::assertSame($elements, $this->sut->getKeyElements(), 'No element has been added.');
+        
+        $this->sut->addUniqueElement($this->createUniqueElementDummy());
+        $this->sut->addKeyRefElement($this->createKeyRefElementDummy());
+        self::assertSame($elements, $this->sut->getKeyElements(), 'Added elements but no KeyElement element.');
+        
+        $elements[] = $this->createKeyElementDummy();
+        $this->sut->addKeyElement($elements[0]);
+        self::assertSame($elements, $this->sut->getKeyElements(), 'Added 1 KeyElement element.');
+        
+        $this->sut->addKeyRefElement($this->createKeyRefElementDummy());
+        self::assertSame($elements, $this->sut->getKeyElements(), 'Added 1 element between.');
+        
+        $elements[] = $this->createKeyElementDummy();
+        $this->sut->addKeyElement($elements[1]);
+        self::assertSame($elements, $this->sut->getKeyElements(), 'Added 2 KeyElement elements.');
+    }
+    
+    /**
+     * Tests that getKeyRefElements() returns:
+     * - an empty array when no element has been added
+     * - an empty array when no KeyRefElement element has been added
+     * - an indexed array of all added KeyRefElement elements
+     * 
+     * @group   elt-content
+     */
+    public function testGetKeyRefElements()
+    {
+        $elements = [];
+        
+        self::assertSame($elements, $this->sut->getKeyRefElements(), 'No element has been added.');
+        
+        $this->sut->addUniqueElement($this->createUniqueElementDummy());
+        $this->sut->addKeyElement($this->createKeyElementDummy());
+        self::assertSame($elements, $this->sut->getKeyRefElements(), 'Added elements but no KeyRefElement element.');
+        
+        $elements[] = $this->createKeyRefElementDummy();
+        $this->sut->addKeyRefElement($elements[0]);
+        self::assertSame($elements, $this->sut->getKeyRefElements(), 'Added 1 KeyRefElement element.');
+        
+        $this->sut->addUniqueElement($this->createUniqueElementDummy());
+        self::assertSame($elements, $this->sut->getKeyRefElements(), 'Added 1 element between.');
+        
+        $elements[] = $this->createKeyRefElementDummy();
+        $this->sut->addKeyRefElement($elements[1]);
+        self::assertSame($elements, $this->sut->getKeyRefElements(), 'Added 2 KeyRefElement elements.');
+    }
+    
+    /**
+     * Tests that getIdentityConstraintElements() returns:
+     * - an empty array when no element has been added
+     * - an indexed array of all added identity-constraint elements
+     * 
+     * @group   elt-content
+     */
+    public function testGetIdentityConstraintElements()
+    {
+        $elements = [];
+        
+        self::assertSame($elements, $this->sut->getIdentityConstraintElements(), 'No element has been added.');
+        
+        $elements[] = $this->createUniqueElementDummy();
+        $elements[] = $this->createKeyElementDummy();
+        $elements[] = $this->createKeyRefElementDummy();
+        $elements[] = $this->createUniqueElementDummy();
+        $elements[] = $this->createKeyElementDummy();
+        $elements[] = $this->createKeyRefElementDummy();
+        $this->sut->addUniqueElement($elements[0]);
+        $this->sut->addKeyElement($elements[1]);
+        $this->sut->addKeyRefElement($elements[2]);
+        $this->sut->addUniqueElement($elements[3]);
+        $this->sut->addKeyElement($elements[4]);
+        $this->sut->addKeyRefElement($elements[5]);
+        self::assertSame($elements, $this->sut->getIdentityConstraintElements(), 'Added 6 identity-constraint elements.');
+    }
+    
+    /**
+     * Tests that getElements() returns an indexed array of all added 
+     * elements in container 2 ((unique | key | keyref)*).
+     * 
+     * @group   elt-content
+     */
+    public function testGetElementsReturnsElementsOfContainer2()
+    {
+        $children = [];
+        $children[] = $this->createUniqueElementDummy();
+        $children[] = $this->createKeyElementDummy();
+        $children[] = $this->createKeyRefElementDummy();
+        $children[] = $this->createUniqueElementDummy();
+        $children[] = $this->createKeyElementDummy();
+        $children[] = $this->createKeyRefElementDummy();
+        $this->sut->addUniqueElement($children[0]);
+        $this->sut->addKeyElement($children[1]);
+        $this->sut->addKeyRefElement($children[2]);
+        $this->sut->addUniqueElement($children[3]);
+        $this->sut->addKeyElement($children[4]);
+        $this->sut->addKeyRefElement($children[5]);
+        self::assertSame($children, $this->sut->getElements(), 'Elements in container 2.');
+    }
 }
