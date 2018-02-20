6276604db62866c48054c49b034f124f2ad85af9
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 22:40:16 +0100

    "any" element is a particle element.

--- src/PhpXmlSchema/Dom/AnyElement.php
+++ src/PhpXmlSchema/Dom/AnyElement.php
@@ -15,6 +15,6 @@ namespace PhpXmlSchema\Dom;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class AnyElement extends AbstractAnnotatedElement
+class AnyElement extends AbstractAnnotatedElement implements ParticleElementInterface
 {
 }
