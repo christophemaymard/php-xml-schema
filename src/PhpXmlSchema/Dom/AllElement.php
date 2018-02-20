e2402769e7edbd1198b9d39c0b9b0edc1ffc0603
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 04:31:19 +0100

    Create the "all" element.

---
+++ src/PhpXmlSchema/Dom/AllElement.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Represents the XML schema "all" element.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class AllElement extends AbstractCompositeElement
+{
+}
---
+++ test/PhpXmlSchema/Test/Unit/Dom/AllElementTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PhpXmlSchema\Dom\AllElement;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\AllElement} class.
+ * 
+ * @group   element
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class AllElementTest extends AbstractCompositeElementTestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->sut = new AllElement();
+    }
+}
