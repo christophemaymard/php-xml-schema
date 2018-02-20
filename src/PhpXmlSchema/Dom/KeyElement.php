e06f9020a6da37cae907266c974cef20790cb17f
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 08:59:10 +0100

    Manage an "annotation" element.

--- src/PhpXmlSchema/Dom/KeyElement.php
+++ src/PhpXmlSchema/Dom/KeyElement.php
@@ -10,8 +10,40 @@ namespace PhpXmlSchema\Dom;
 /**
  * Represents the XML schema "key" element.
  * 
+ * Content (version 1.0):
+ * (annotation?, (selector, field+))
+ * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
 class KeyElement extends AbstractCompositeElement
 {
+    /**
+     * Returns the "annotation" element.
+     * 
+     * @return  AnnotationElement|NULL  The instance of the element if it has been set, otherwise NULL.
+     */
+    public function getAnnotationElement()
+    {
+        return $this->getChildElement(0);
+    }
+    
+    /**
+     * Sets the "annotation" element.
+     * 
+     * @param   AnnotationElement   $element    The element to set.
+     */
+    public function setAnnotationElement(AnnotationElement $element)
+    {
+        $this->setChildElement(0, $element);
+    }
+    
+    /**
+     * Indicates whether an "annotation" element has been set.
+     * 
+     * @return  bool    TRUE if an element has been set, otherwise FALSE.
+     */
+    public function hasAnnotationElement():bool
+    {
+        return $this->isChildElementSet(0);
+    }
 }
