74b1c296613e7a2d73ce1ff92649c28b4cf38228
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 05:21:24 +0100

    Create the "maxLength" element.

---
+++ test/PhpXmlSchema/Test/Unit/Dom/MaxLengthElementTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PhpXmlSchema\Dom\MaxLengthElement;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\MaxLengthElement} 
+ * class.
+ * 
+ * @group   element
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class MaxLengthElementTest extends AbstractCompositeElementTestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->sut = new MaxLengthElement();
+    }
+}
