08a2df8e6a1728a844e31f82b19a79a4c922e12c
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 05:32:32 +0100

    Create the "schema" element.

---
+++ src/PhpXmlSchema/Dom/SchemaElement.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Represents the XML schema "schema" element.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class SchemaElement extends AbstractCompositeElement
+{
+}
