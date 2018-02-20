702023b6e19b7141bf13425c785a9fde79d9eb11
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 06:36:31 +0100

    Manage "appinfo" and "documentation" elements.

--- src/PhpXmlSchema/Dom/AnnotationElement.php
+++ src/PhpXmlSchema/Dom/AnnotationElement.php
@@ -10,8 +10,50 @@ namespace PhpXmlSchema\Dom;
 /**
  * Represents the XML schema "annotation" element.
  * 
+ * Content (version 1.0):
+ * (appinfo | documentation)*
+ * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
 class AnnotationElement extends AbstractCompositeElement
 {
+    /**
+     * Adds an "appinfo" element to this element.
+     * 
+     * @param   AppInfoElement  $element    The element to add.
+     */
+    public function addAppInfoElement(AppInfoElement $element)
+    {
+        $this->addChildElement(0, $element);
+    }
+    
+    /**
+     * Returns all the "appinfo" child elements.
+     * 
+     * @return  AppInfoElement[]    An indexed array of AppInfoElement instances.
+     */
+    public function getAppInfoElements():array
+    {
+        return $this->getChildElementsByType(0, AppInfoElement::class);
+    }
+    
+    /**
+     * Adds a "documentation" element to this element.
+     * 
+     * @param   DocumentationElement    $element    The element to add.
+     */
+    public function addDocumentationElement(DocumentationElement $element)
+    {
+        $this->addChildElement(0, $element);
+    }
+    
+    /**
+     * Returns all the "documentation" child elements.
+     * 
+     * @return  DocumentationElement[]  An indexed array of DocumentationElement instances.
+     */
+    public function getDocumentationElements():array
+    {
+        return $this->getChildElementsByType(0, DocumentationElement::class);
+    }
 }
--- test/PhpXmlSchema/Test/Unit/Dom/AnnotationElementTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/AnnotationElementTest.php
@@ -27,4 +27,82 @@ class AnnotationElementTest extends AbstractCompositeElementTestCase
     {
         $this->sut = new AnnotationElement();
     }
+    
+    /**
+     * Tests that getAppInfoElements() returns:
+     * - an empty array when no element has been added
+     * - an empty array when no AppInfoElement element has been added
+     * - an indexed array of all added AppInfoElement elements
+     * 
+     * @group   elt-content
+     */
+    public function testGetAppInfoElements()
+    {
+        $elements = [];
+        
+        self::assertSame($elements, $this->sut->getAppInfoElements(), 'No element has been added.');
+        
+        $this->sut->addDocumentationElement($this->createDocumentationElementDummy());
+        self::assertSame($elements, $this->sut->getAppInfoElements(), 'Added elements but no AppInfoElement element.');
+        
+        $elements[] = $this->createAppInfoElementDummy();
+        $this->sut->addAppInfoElement($elements[0]);
+        self::assertSame($elements, $this->sut->getAppInfoElements(), 'Added 1 AppInfoElement element.');
+        
+        $this->sut->addDocumentationElement($this->createDocumentationElementDummy());
+        self::assertSame($elements, $this->sut->getAppInfoElements(), 'Added 1 element between.');
+        
+        $elements[] = $this->createAppInfoElementDummy();
+        $this->sut->addAppInfoElement($elements[1]);
+        self::assertSame($elements, $this->sut->getAppInfoElements(), 'Added 2 AppInfoElement elements.');
+    }
+    
+    /**
+     * Tests that getDocumentationElements() returns:
+     * - an empty array when no element has been added
+     * - an empty array when no DocumentationElement element has been added
+     * - an indexed array of all added DocumentationElement elements
+     * 
+     * @group   elt-content
+     */
+    public function testGetDocumentationElements()
+    {
+        $elements = [];
+        
+        self::assertSame($elements, $this->sut->getDocumentationElements(), 'No element has been added.');
+        
+        $this->sut->addAppInfoElement($this->createAppInfoElementDummy());
+        self::assertSame($elements, $this->sut->getDocumentationElements(), 'Added elements but no DocumentationElement element.');
+        
+        $elements[] = $this->createDocumentationElementDummy();
+        $this->sut->addDocumentationElement($elements[0]);
+        self::assertSame($elements, $this->sut->getDocumentationElements(), 'Added 1 DocumentationElement element.');
+        
+        $this->sut->addAppInfoElement($this->createAppInfoElementDummy());
+        self::assertSame($elements, $this->sut->getDocumentationElements(), 'Added 1 element between.');
+        
+        $elements[] = $this->createDocumentationElementDummy();
+        $this->sut->addDocumentationElement($elements[1]);
+        self::assertSame($elements, $this->sut->getDocumentationElements(), 'Added 2 DocumentationElement elements.');
+    }
+    
+    /**
+     * Tests that getElements() returns an indexed array of all added 
+     * elements in container 0 ((appinfo | documentation)*).
+     * 
+     * @group   elt-content
+     */
+    public function testGetElementsReturnsElementsOfContainer0()
+    {
+        $children = [];
+        $children[] = $this->createAppInfoElementDummy();
+        $children[] = $this->createDocumentationElementDummy();
+        $children[] = $this->createAppInfoElementDummy();
+        $children[] = $this->createDocumentationElementDummy();
+        $this->sut->addAppInfoElement($children[0]);
+        $this->sut->addDocumentationElement($children[1]);
+        $this->sut->addAppInfoElement($children[2]);
+        $this->sut->addDocumentationElement($children[3]);
+        self::assertSame($children, $this->sut->getElements(), 'Elements in container 0.');
+    }
 }
