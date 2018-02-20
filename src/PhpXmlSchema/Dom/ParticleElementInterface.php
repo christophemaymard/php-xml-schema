928bc8a011b56b0fca2fca1da85749f7321e984b
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 22:33:07 +0100

    Introduce particle elements.

---
+++ src/PhpXmlSchema/Dom/ParticleElementInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Interface for a XML schema element that contributes to the definition of 
+ * content models.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+interface ParticleElementInterface extends AnnotatedElementInterface
+{
+}
