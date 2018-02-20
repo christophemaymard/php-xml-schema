2b309fb1c4208b1ca21aeb62be769b54df3eeb2d
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 04:39:32 +0100

    Create the "attributeGroup" element.

---
+++ src/PhpXmlSchema/Dom/AttributeGroupElement.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Represents the XML schema "attributeGroup" element.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class AttributeGroupElement extends AbstractCompositeElement
+{
+}
