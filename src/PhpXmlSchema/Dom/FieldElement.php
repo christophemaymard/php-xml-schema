4936024db03c07a9753476af57cd2a1db0acb4c9
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 04:54:46 +0100

    Create the "field" element.

---
+++ src/PhpXmlSchema/Dom/FieldElement.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Represents the XML schema "field" element.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class FieldElement extends AbstractCompositeElement
+{
+}
