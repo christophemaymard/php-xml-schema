29b26523b228d69c23436671536cfac16338397c
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 05:38:23 +0100

    Create the "simpleContent" element.

---
+++ test/PhpXmlSchema/Test/Unit/Dom/SimpleContentElementTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PhpXmlSchema\Dom\SimpleContentElement;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\SimpleContentElement} 
+ * class.
+ * 
+ * @group   element
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class SimpleContentElementTest extends AbstractCompositeElementTestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->sut = new SimpleContentElement();
+    }
+}
