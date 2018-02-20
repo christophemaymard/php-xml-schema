2fcc335f6ae8457574d53c07a0d9bae57f8af238
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 20:42:59 +0100

    Introduce simple type derivation elements.

---
+++ src/PhpXmlSchema/Dom/SimpleTypeDerivationElementInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Interface for a XML schema element that represents a derivation in the 
+ * "simpleType" element ({@see PhpXmlSchema\Dom\SimpleTypeElement}).
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+interface SimpleTypeDerivationElementInterface extends AnnotatedElementInterface
+{
+}
