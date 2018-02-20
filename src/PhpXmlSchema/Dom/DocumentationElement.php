c48a146001930e4537db6ed438d526a330e2189a
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-20 04:14:39 +0100

    Create the "documentation" element.

---
+++ src/PhpXmlSchema/Dom/DocumentationElement.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Represents the XML schema "documentation" element.
+ * 
+ * Content (version 1.0):
+ * ({any}*)
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DocumentationElement extends AbstractLeafElement
+{
+}
