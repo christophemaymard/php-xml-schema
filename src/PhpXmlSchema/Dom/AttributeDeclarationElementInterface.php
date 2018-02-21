d0979c4f4a0cb133a5390dbb6eb9d0b73de781b3
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-21 13:20:47 +0100

    Introduce attribute declaration elements.

---
+++ src/PhpXmlSchema/Dom/AttributeDeclarationElementInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Interface for a XML schema element that declares an attribute or a group 
+ * of attributes.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+interface AttributeDeclarationElementInterface extends AnnotatedElementInterface
+{
+}
