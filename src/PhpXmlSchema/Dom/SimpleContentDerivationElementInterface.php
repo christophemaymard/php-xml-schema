04caf6fa8f5c9132355cdf4181196f4a31736634
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-21 17:59:51 +0100

    Introduce simple content derivation elements.

---
+++ src/PhpXmlSchema/Dom/SimpleContentDerivationElementInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Interface for a XML schema element that represents a derivation in the 
+ * "simpleContent" element ({@see PhpXmlSchema\Dom\SimpleContentElement}).
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+interface SimpleContentDerivationElementInterface extends AttributeNamingElementInterface
+{
+}
