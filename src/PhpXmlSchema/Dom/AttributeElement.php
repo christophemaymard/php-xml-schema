b8c1082a1509084dfb4387df3f8d8166e5583e09
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-21 13:23:11 +0100

    "attribute" element is an attribute declaration element.

--- src/PhpXmlSchema/Dom/AttributeElement.php
+++ src/PhpXmlSchema/Dom/AttributeElement.php
@@ -15,6 +15,6 @@ namespace PhpXmlSchema\Dom;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class AttributeElement extends AbstractSimpleTypedElement
+class AttributeElement extends AbstractSimpleTypedElement implements AttributeDeclarationElementInterface
 {
 }
