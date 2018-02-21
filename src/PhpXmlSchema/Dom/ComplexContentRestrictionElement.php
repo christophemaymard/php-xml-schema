752ed35488b619d238752b1e89033f6273b46020
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-21 13:51:41 +0100

    Manage a type definition particle element.

--- src/PhpXmlSchema/Dom/ComplexContentRestrictionElement.php
+++ src/PhpXmlSchema/Dom/ComplexContentRestrictionElement.php
@@ -47,4 +47,34 @@ class ComplexContentRestrictionElement extends AbstractCompositeElement
     {
         return $this->isChildElementSet(0);
     }
+    
+    /**
+     * Returns the type definition particle element.
+     * 
+     * @return  TypeDefinitionParticleElementInterface|NULL The instance of the element if it has been set, otherwise NULL.
+     */
+    public function getTypeDefinitionParticleElement()
+    {
+        return $this->getChildElement(1);
+    }
+    
+    /**
+     * Sets the type definition particle element.
+     * 
+     * @param   TypeDefinitionParticleElementInterface  $element    The element to set.
+     */
+    public function setTypeDefinitionParticleElement(TypeDefinitionParticleElementInterface $element)
+    {
+        $this->setChildElement(1, $element);
+    }
+    
+    /**
+     * Indicates whether a type definition particle element has been set.
+     * 
+     * @return  bool    TRUE if an element has been set, otherwise FALSE.
+     */
+    public function hasTypeDefinitionParticleElement():bool
+    {
+        return $this->isChildElementSet(1);
+    }
 }
--- test/PhpXmlSchema/Test/Unit/Dom/ComplexContentRestrictionElementTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/ComplexContentRestrictionElementTest.php
@@ -76,4 +76,61 @@ class ComplexContentRestrictionElementTest extends AbstractCompositeElementTestC
         $this->sut->setAnnotationElement($children[0]);
         self::assertSame($children, $this->sut->getElements(), 'Elements in container 0.');
     }
+    
+    /**
+     * Tests that hasTypeDefinitionParticleElement() returns a boolean:
+     * - FALSE when no element has been set
+     * - TRUE when an element has been set
+     * 
+     * @group   elt-content
+     */
+    public function testHasTypeDefinitionParticleElement()
+    {
+        self::assertFalse($this->sut->hasTypeDefinitionParticleElement(), 'No element has been set.');
+        
+        $this->sut->setTypeDefinitionParticleElement($this->createTypeDefinitionParticleElementInterfaceDummy());
+        self::assertTrue($this->sut->hasTypeDefinitionParticleElement(), 'Set with an element: TypeDefinitionParticleElementInterface.');
+    }
+    
+    /**
+     * Tests that getTypeDefinitionParticleElement() returns:
+     * - NULL when no element has been set
+     * - the instance of the element that has been set
+     * 
+     * @group   elt-content
+     */
+    public function testGetTypeDefinitionParticleElement()
+    {
+        self::assertNull($this->sut->getTypeDefinitionParticleElement(), 'No element has been set.');
+        
+        $elt1 = $this->createGroupElementDummy();
+        $this->sut->setTypeDefinitionParticleElement($elt1);
+        self::assertSame($elt1, $this->sut->getTypeDefinitionParticleElement(), 'Set with an element: GroupElement.');
+        
+        $elt2 = $this->createAllElementDummy();
+        $this->sut->setTypeDefinitionParticleElement($elt2);
+        self::assertSame($elt2, $this->sut->getTypeDefinitionParticleElement(), 'Set with another element: AllElement.');
+        
+        $elt3 = $this->createChoiceElementDummy();
+        $this->sut->setTypeDefinitionParticleElement($elt3);
+        self::assertSame($elt3, $this->sut->getTypeDefinitionParticleElement(), 'Set with another element: ChoiceElement.');
+        
+        $elt4 = $this->createSequenceElementDummy();
+        $this->sut->setTypeDefinitionParticleElement($elt4);
+        self::assertSame($elt4, $this->sut->getTypeDefinitionParticleElement(), 'Set with another element: SequenceElement.');
+    }
+    
+    /**
+     * Tests that getElements() returns an indexed array of all added 
+     * elements in container 1 ((group | all | choice | sequence)?).
+     * 
+     * @group   elt-content
+     */
+    public function testGetElementsReturnsElementsOfContainer1()
+    {
+        $children = [];
+        $children[] = $this->createTypeDefinitionParticleElementInterfaceDummy();
+        $this->sut->setTypeDefinitionParticleElement($children[0]);
+        self::assertSame($children, $this->sut->getElements(), 'Elements in container 1.');
+    }
 }
