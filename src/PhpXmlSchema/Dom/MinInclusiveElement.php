7a58603d1bda1819db894e7c39f75179f78414cc
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-21 11:52:56 +0100

    "minInclusive" element is a facet element.

--- src/PhpXmlSchema/Dom/MinInclusiveElement.php
+++ src/PhpXmlSchema/Dom/MinInclusiveElement.php
@@ -15,6 +15,6 @@ namespace PhpXmlSchema\Dom;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class MinInclusiveElement extends AbstractAnnotatedElement
+class MinInclusiveElement extends AbstractAnnotatedElement implements FacetElementInterface
 {
 }
