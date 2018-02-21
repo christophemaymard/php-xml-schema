ccccbfeadc01d1f99826c10f59ff3d6997af4264
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-21 11:53:28 +0100

    "minLength" element is a facet element.

--- src/PhpXmlSchema/Dom/MinLengthElement.php
+++ src/PhpXmlSchema/Dom/MinLengthElement.php
@@ -15,6 +15,6 @@ namespace PhpXmlSchema\Dom;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class MinLengthElement extends AbstractAnnotatedElement
+class MinLengthElement extends AbstractAnnotatedElement implements FacetElementInterface
 {
 }
