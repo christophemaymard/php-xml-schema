87c39d738f8cee54da163bc14dd24513368cef77
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-21 15:50:53 +0100

    Add a test for the sequence of the containers.

--- test/PhpXmlSchema/Test/Unit/Dom/ComplexContentRestrictionElementTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/ComplexContentRestrictionElementTest.php
@@ -267,6 +267,45 @@ class ComplexContentRestrictionElementTest extends AbstractCompositeElementTestC
     }
     
     /**
+     * Tests that getElements() returns an indexed array of all added 
+     * elements in that order:
+     * - elements from container 0 (annotation?)
+     * - elements from container 1 ((group | all | choice | sequence)?)
+     * - elements from container 2 ((attribute | attributeGroup)*)
+     * - elements from container 3 (anyAttribute?)
+     * 
+     * @group   elt-content
+     */
+    public function testGetElementsReturnsElementsOrderedByContainer0123()
+    {
+        $children = [];
+        $children[] = $this->createAnnotationElementDummy();
+        $children[] = $this->createTypeDefinitionParticleElementInterfaceDummy();
+        $children[] = $this->createAttributeElementDummy();
+        $children[] = $this->createAttributeGroupElementDummy();
+        $children[] = $this->createAttributeElementDummy();
+        $children[] = $this->createAttributeGroupElementDummy();
+        $children[] = $this->createAnyAttributeElementDummy();
+        
+        // Init container 3.
+        $this->sut->setAnyAttributeElement($children[6]);
+        
+        // Init container 2.
+        $this->sut->addAttributeElement($children[2]);
+        $this->sut->addAttributeGroupElement($children[3]);
+        $this->sut->addAttributeElement($children[4]);
+        $this->sut->addAttributeGroupElement($children[5]);
+        
+        // Init container 1.
+        $this->sut->setTypeDefinitionParticleElement($children[1]);
+        
+        // Init container 0.
+        $this->sut->setAnnotationElement($children[0]);
+        
+        self::assertSame($children, $this->sut->getElements(), 'Elements ordered by container: 0, 1, 2, 3.');
+    }
+    
+    /**
      * Fills the container 2 ((attribute | attributeGroup)*) of the SUT with 
      * a set of elements.
      * 
