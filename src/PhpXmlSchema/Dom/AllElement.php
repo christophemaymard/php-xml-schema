89ea15e2f195587df1ad327a2cf88b6605f86648
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-21 09:38:45 +0100

    "all" element is a model group element.

--- src/PhpXmlSchema/Dom/AllElement.php
+++ src/PhpXmlSchema/Dom/AllElement.php
@@ -15,59 +15,6 @@ namespace PhpXmlSchema\Dom;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class AllElement extends AbstractCompositeElement implements ParticleElementInterface
+class AllElement extends AbstractModelGroupElement implements ParticleElementInterface
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function getAnnotationElement()
-    {
-        return $this->getChildElement(0);
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    public function setAnnotationElement(AnnotationElement $element)
-    {
-        $this->setChildElement(0, $element);
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    public function hasAnnotationElement():bool
-    {
-        return $this->isChildElementSet(0);
-    }
-    
-    /**
-     * Adds an "element" element to this element.
-     * 
-     * @param   ElementElement  $element    The element to add.
-     */
-    public function addElementElement(ElementElement $element)
-    {
-        $this->addChildElement(1, $element);
-    }
-    
-    /**
-     * Returns all the "element" child elements.
-     * 
-     * @return  ElementElement[]    An indexed array of ElementElement instances.
-     */
-    public function getElementElements():array
-    {
-        return $this->getChildElementsByType(1, ElementElement::class);
-    }
-    
-    /**
-     * Returns all the particle child elements.
-     * 
-     * @return  ParticleElementInterface[]  An indexed array of ParticleElementInterface instances.
-     */
-    public function getParticleElements():array
-    {
-        return $this->getChildElementsByType(1, ParticleElementInterface::class);
-    }
 }
--- test/PhpXmlSchema/Test/Unit/Dom/AllElementTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/AllElementTest.php
@@ -18,7 +18,7 @@ use Prophecy\Prophecy\ProphecySubjectInterface;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class AllElementTest extends AbstractCompositeElementTestCase
+class AllElementTest extends AbstractModelGroupElementTestCase
 {
     /**
      * {@inheritDoc}
@@ -26,55 +26,6 @@ class AllElementTest extends AbstractCompositeElementTestCase
     protected function setUp()
     {
         $this->sut = new AllElement();
-    }
-    
-    /**
-     * Tests that hasAnnotationElement() returns a boolean:
-     * - FALSE when no element has been set
-     * - TRUE when an element has been set
-     * 
-     * @group   elt-content
-     */
-    public function testHasAnnotationElement()
-    {
-        self::assertFalse($this->sut->hasAnnotationElement(), 'No element has been set.');
-        
-        $this->sut->setAnnotationElement($this->createAnnotationElementDummy());
-        self::assertTrue($this->sut->hasAnnotationElement(), 'Set with an element: AnnotationElement.');
-    }
-    
-    /**
-     * Tests that getAnnotationElement() returns:
-     * - NULL when no element has been set
-     * - the instance of the element that has been set
-     * 
-     * @group   elt-content
-     */
-    public function testGetAnnotationElement()
-    {
-        self::assertNull($this->sut->getAnnotationElement(), 'No element has been set.');
-        
-        $elt1 = $this->createAnnotationElementDummy();
-        $this->sut->setAnnotationElement($elt1);
-        self::assertSame($elt1, $this->sut->getAnnotationElement(), 'Set with an element: AnnotationElement.');
-        
-        $elt2 = $this->createAnnotationElementDummy();
-        $this->sut->setAnnotationElement($elt2);
-        self::assertSame($elt2, $this->sut->getAnnotationElement(), 'Set with another element: AnnotationElement.');
-    }
-    
-    /**
-     * Tests that getElements() returns an indexed array of all added 
-     * elements in container 0 (annotation?).
-     * 
-     * @group   elt-content
-     */
-    public function testGetElementsReturnsElementsOfContainer0()
-    {
-        $children = [];
-        $children[] = $this->createAnnotationElementDummy();
-        $this->sut->setAnnotationElement($children[0]);
-        self::assertSame($children, $this->sut->getElements(), 'Elements in container 0.');
     }
     
     /**
@@ -97,31 +48,6 @@ class AllElementTest extends AbstractCompositeElementTestCase
         $elements[] = $this->createElementElementDummy();
         $this->sut->addElementElement($elements[1]);
         self::assertSame($elements, $this->sut->getElementElements(), 'Added 2 ElementElement elements.');
-    }
-    
-    /**
-     * Tests that getParticleElements() returns:
-     * - an empty array when no element has been added
-     * - an indexed array of all added particle elements in container 1 (element*)
-     * 
-     * @group   elt-content
-     */
-    public function testGetParticleElements()
-    {
-        self::assertSame([], $this->sut->getParticleElements(), 'No element has been added.');
-        
-        self::assertSame($this->fillSutContainer1(), $this->sut->getParticleElements(), 'Added particle elements.');
-    }
-    
-    /**
-     * Tests that getElements() returns an indexed array of all added 
-     * elements in container 1 (element*).
-     * 
-     * @group   elt-content
-     */
-    public function testGetElementsReturnsElementsOfContainer1()
-    {
-        self::assertSame($this->fillSutContainer1(), $this->sut->getElements(), 'Elements in container 1.');
     }
     
     /**
@@ -154,7 +80,7 @@ class AllElementTest extends AbstractCompositeElementTestCase
      * 
      * @return  ProphecySubjectInterface[]  An indexed array of all the created elements.
      */
-    private function fillSutContainer1():array
+    protected function fillSutContainer1():array
     {
         $elements = [];
         $elements[] = $this->createElementElementDummy();
