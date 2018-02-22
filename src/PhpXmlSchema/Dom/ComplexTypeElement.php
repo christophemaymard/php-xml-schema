80b71e0d0e33be9ef82e843e1d7b426f2e041877
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-22 11:02:51 +0100

    Manage a content element.

--- src/PhpXmlSchema/Dom/ComplexTypeElement.php
+++ src/PhpXmlSchema/Dom/ComplexTypeElement.php
@@ -17,4 +17,33 @@ namespace PhpXmlSchema\Dom;
  */
 class ComplexTypeElement extends AbstractTypeNamingElement implements TypeElementInterface
 {
+    /**
+     * Returns the content element.
+     * 
+     * @return  ContentElementInterface|NULL    The instance of the element if it has been set, otherwise NULL.
+     */
+    public function getContentElement()
+    {
+        return $this->getChildElement(1);
+    }
+    
+    /**
+     * Sets the content element.
+     * 
+     * @param   ContentElementInterface $element    The element to set.
+     */
+    public function setContentElement(ContentElementInterface $element)
+    {
+        $this->setChildElement(1, $element);
+    }
+    
+    /**
+     * Indicates whether a content element has been set.
+     * 
+     * @return  bool    TRUE if an element has been set, otherwise FALSE.
+     */
+    public function hasContentElement():bool
+    {
+        return $this->isChildElementSet(1);
+    }
 }
--- test/PhpXmlSchema/Test/Unit/Dom/ComplexTypeElementTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/ComplexTypeElementTest.php
@@ -27,4 +27,53 @@ class ComplexTypeElementTest extends AbstractAbstractTypeNamingElementTestCase
     {
         $this->sut = new ComplexTypeElement();
     }
+    
+    /**
+     * Tests that hasContentElement() returns a boolean:
+     * - FALSE when no element has been set
+     * - TRUE when an element has been set
+     * 
+     * @group   elt-content
+     */
+    public function testHasContentElement()
+    {
+        self::assertFalse($this->sut->hasContentElement(), 'No element has been set.');
+        
+        $this->sut->setContentElement($this->createContentElementInterfaceDummy());
+        self::assertTrue($this->sut->hasContentElement(), 'Set with an element: ContentElementInterface.');
+    }
+    
+    /**
+     * Tests that getContentElement() returns:
+     * - NULL when no element has been set
+     * - the instance of the element that has been set
+     * 
+     * @group   elt-content
+     */
+    public function testGetContentElement()
+    {
+        self::assertNull($this->sut->getContentElement(), 'No element has been set.');
+        
+        $elt1 = $this->createSimpleContentElementDummy();
+        $this->sut->setContentElement($elt1);
+        self::assertSame($elt1, $this->sut->getContentElement(), 'Set with an element: SimpleContentElement.');
+        
+        $elt2 = $this->createComplexContentElementDummy();
+        $this->sut->setContentElement($elt2);
+        self::assertSame($elt2, $this->sut->getContentElement(), 'Set with another element: ComplexContentElement.');
+    }
+    
+    /**
+     * Tests that getElements() returns an indexed array of all added 
+     * elements in container 1 ((simpleContent | complexContent)).
+     * 
+     * @group   elt-content
+     */
+    public function testGetElementsReturnsElementsOfContainer1()
+    {
+        $children = [];
+        $children[] = $this->createContentElementInterfaceDummy();
+        $this->sut->setContentElement($children[0]);
+        self::assertSame($children, $this->sut->getElements(), 'Elements in container 1.');
+    }
 }
