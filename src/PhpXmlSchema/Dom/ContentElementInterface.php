0b48b26f161da0d14b178e8e9d3270bad2044587
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-22 10:53:21 +0100

    Introduce content elements.

---
+++ src/PhpXmlSchema/Dom/ContentElementInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Interface for a XML schema element that represents a content in the 
+ * "complexType" element ({@see PhpXmlSchema\Dom\ComplexTypeElement}).
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+interface ContentElementInterface extends AnnotatedElementInterface
+{
+}
