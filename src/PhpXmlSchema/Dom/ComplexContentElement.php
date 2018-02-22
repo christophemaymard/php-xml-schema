1a3e31687413197bf6582a9ac383934a6f44960c
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-22 10:20:34 +0100

    "complexContent" element is an annotated element.

--- src/PhpXmlSchema/Dom/ComplexContentElement.php
+++ src/PhpXmlSchema/Dom/ComplexContentElement.php
@@ -15,35 +15,6 @@ namespace PhpXmlSchema\Dom;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class ComplexContentElement extends AbstractCompositeElement
+class ComplexContentElement extends AbstractAnnotatedElement
 {
-    /**
-     * Returns the "annotation" element.
-     * 
-     * @return  AnnotationElement|NULL  The instance of the element if it has been set, otherwise NULL.
-     */
-    public function getAnnotationElement()
-    {
-        return $this->getChildElement(0);
-    }
-    
-    /**
-     * Sets the "annotation" element.
-     * 
-     * @param   AnnotationElement   $element    The element to set.
-     */
-    public function setAnnotationElement(AnnotationElement $element)
-    {
-        $this->setChildElement(0, $element);
-    }
-    
-    /**
-     * Indicates whether an "annotation" element has been set.
-     * 
-     * @return  bool    TRUE if an element has been set, otherwise FALSE.
-     */
-    public function hasAnnotationElement():bool
-    {
-        return $this->isChildElementSet(0);
-    }
 }
