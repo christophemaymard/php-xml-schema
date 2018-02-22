67da835ceb9d47b48a57774c7e8636dbf7072c0d
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-22 10:37:09 +0100

    "extension" element (complexContent) is a complex content derivation element.

--- src/PhpXmlSchema/Dom/ComplexContentExtensionElement.php
+++ src/PhpXmlSchema/Dom/ComplexContentExtensionElement.php
@@ -16,6 +16,7 @@ namespace PhpXmlSchema\Dom;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class ComplexContentExtensionElement extends AbstractTypeNamingElement
+class ComplexContentExtensionElement extends AbstractTypeNamingElement implements
+    ComplexContentDerivationElementInterface
 {
 }
