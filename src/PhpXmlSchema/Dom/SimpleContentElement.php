2f83ee9cdb8563f613b72b8e45ec8fbd6c0d65e7
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-22 10:58:45 +0100

    "simpleContent" element is a content element.

--- src/PhpXmlSchema/Dom/SimpleContentElement.php
+++ src/PhpXmlSchema/Dom/SimpleContentElement.php
@@ -15,7 +15,7 @@ namespace PhpXmlSchema\Dom;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class SimpleContentElement extends AbstractAnnotatedElement
+class SimpleContentElement extends AbstractAnnotatedElement implements ContentElementInterface
 {
     /**
      * Returns the derivation element.
