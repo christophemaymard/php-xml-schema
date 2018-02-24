07dd6b89fcc692e5290d20b3478fc009df3079ee
Christophe Maymard <christophe.maymard@hotmail.com>
2018-02-24 11:45:05 +0100

    Introduce exceptions.

---
+++ src/PhpXmlSchema/Exception/PhpXmlSchemaExceptionInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Exception;
+
+/**
+ * Interface for all the exceptions.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+interface PhpXmlSchemaExceptionInterface extends \Throwable
+{
+}
