b0355c588746e44b1cc422ae79eb8ec27ef52c76
christophe.maymard <christophe.maymard@hotmail.com>
2018-09-05 15:27:52 +0200

    Create the "processingMode" type.

---
+++ src/PhpXmlSchema/Dom/ProcessingModeType.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+use PhpXmlSchema\Exception\InvalidValueException;
+
+/**
+ * Represents the type for the mode of content processing.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ProcessingModeType
+{
+    /**
+     * XML processor validates elements and attributes for which it can 
+     * obtain schema information, but it will not signal errors for those it 
+     * cannot obtain any schema information.
+     */
+    const LAX = 1;
+    
+    /**
+     * XML processor does not attempt to validate any elements from the 
+     * specified namespaces.
+     */
+    const SKIP = 2;
+    
+    /**
+     * XML processor must obtain the schema associated with the required 
+     * namespaces and validate the elements.
+     */
+    const STRICT = 3;
+    
+    /**
+     * The mode of the content processing.
+     * @var int
+     */
+    private $mode;
+    
+    /**
+     * Creates a "lax" mode of content processing.
+     * 
+     * @return  ProcessingModeType  A new instance of ProcessingModeType.
+     */
+    public static function createLax():self
+    {
+        return new self(self::LAX);
+    }
+    
+    /**
+     * Creates a "skip" mode of content processing.
+     * 
+     * @return  ProcessingModeType  A new instance of ProcessingModeType.
+     */
+    public static function createSkip():self
+    {
+        return new self(self::SKIP);
+    }
+    
+    /**
+     * Creates a "strict" mode of content processing.
+     * 
+     * @return  ProcessingModeType  A new instance of ProcessingModeType.
+     */
+    public static function createStrict():self
+    {
+        return new self(self::STRICT);
+    }
+    
+    /**
+     * Constructor.
+     * 
+     * @param   int $mode   The mode of the content processing to set.
+     */
+    public function __construct(int $mode)
+    {
+        $this->setMode($mode);
+    }
+    
+    /**
+     * Sets the mode of the content processing.
+     * 
+     * @param   int $mode   The mode of the content processing to set.
+     * 
+     * @throws  InvalidValueException   When the mode of the content processing is an invalid value.
+     */
+    private function setMode(int $mode)
+    {
+        if ($mode != self::LAX && $mode != self::SKIP && $mode != self::STRICT) {
+            throw new InvalidValueException(\sprintf('"%s" is an invalid mode of content processing.', $mode));
+        }
+        
+        $this->mode = $mode;
+    }
+    
+    /**
+     * Indicates whether the mode of the content processing is "lax".
+     * 
+     * @return  bool    TRUE if the mode is "lax", otherwise FALSE.
+     */
+    public function isLax():bool
+    {
+        return $this->mode == self::LAX;
+    }
+    
+    /**
+     * Indicates whether the mode of the content processing is "skip".
+     * 
+     * @return  bool    TRUE if the mode is "skip", otherwise FALSE.
+     */
+    public function isSkip():bool
+    {
+        return $this->mode == self::SKIP;
+    }
+    
+    /**
+     * Indicates whether the mode of the content processing is "strict".
+     * 
+     * @return  bool    TRUE if the mode is "strict", otherwise FALSE.
+     */
+    public function isStrict():bool
+    {
+        return $this->mode == self::STRICT;
+    }
+}
---
+++ test/PhpXmlSchema/Test/Unit/Dom/ProcessingModeTypeTest.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PHPUnit\Framework\TestCase;
+use PhpXmlSchema\Dom\ProcessingModeType;
+use PhpXmlSchema\Exception\InvalidValueException;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\ProcessingModeType} 
+ * class.
+ * 
+ * @group   type
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ProcessingModeTypeTest extends TestCase
+{
+    /**
+     * Asserts that isLax() returns TRUE, isSkip() and isStrict() return 
+     * FALSE.
+     * 
+     * @param   ProcessingModeType  $sut
+     */
+    private static function assertProcessingModeLax(ProcessingModeType $sut)
+    {
+        self::assertTrue($sut->isLax());
+        self::assertFalse($sut->isSkip());
+        self::assertFalse($sut->isStrict());
+    }
+    
+    /**
+     * Asserts that isSkip() returns TRUE, isLax() and isStrict() return 
+     * FALSE.
+     * 
+     * @param   ProcessingModeType  $sut
+     */
+    private static function assertProcessingModeSkip(ProcessingModeType $sut)
+    {
+        self::assertTrue($sut->isSkip());
+        self::assertFalse($sut->isLax());
+        self::assertFalse($sut->isStrict());
+    }
+    
+    /**
+     * Asserts that isStrict() returns TRUE, isLax() and isSkip() return 
+     * FALSE.
+     * 
+     * @param   ProcessingModeType  $sut
+     */
+    private static function assertProcessingModeStrict(ProcessingModeType $sut)
+    {
+        self::assertTrue($sut->isStrict());
+        self::assertFalse($sut->isLax());
+        self::assertFalse($sut->isSkip());
+    }
+    
+    /**
+     * Tests that isLax() returns TRUE, isSkip() and isStrict() return FALSE 
+     * when the specified mode is "lax".
+     */
+    public function testIsLaxReturnsTrueWhenModeLax()
+    {
+        $sut = new ProcessingModeType(1);
+        self::assertProcessingModeLax($sut);
+    }
+    
+    /**
+     * Tests that isSkip() returns TRUE, isLax() and isStrict() return FALSE 
+     * when the specified mode is "skip".
+     */
+    public function testIsSkipReturnsTrueWhenModeSkip()
+    {
+        $sut = new ProcessingModeType(2);
+        self::assertProcessingModeSkip($sut);
+    }
+    
+    /**
+     * Tests that isStrict() returns TRUE, isLax() and isSkip() return FALSE 
+     * when the specified mode is "strict".
+     */
+    public function testIsStrictReturnsTrueWhenModeStrict()
+    {
+        $sut = new ProcessingModeType(3);
+        self::assertProcessingModeStrict($sut);
+    }
+    
+    /**
+     * Tests that __construct() throws an exception when the specified mode 
+     * is invalid.
+     */
+    public function test__constructThrowsExceptionWhenModeIsInvalid()
+    {
+        $this->expectException(InvalidValueException::class);
+        $this->expectExceptionMessage('"4" is an invalid mode of content processing.');
+        
+        $sut = new ProcessingModeType(4);
+    }
+    
+    /**
+     * Tests that createLax() creates a new instance of ProcessingModeType 
+     * configured in "lax" mode.
+     */
+    public function testCreateLaxReturnsInstanceModeLax()
+    {
+        $sut = ProcessingModeType::createLax();
+        self::assertProcessingModeLax($sut);
+    }
+    
+    /**
+     * Tests that createLax() creates a new instance.
+     */
+    public function testCreateLaxReturnsNewInstance()
+    {
+        $sut1 = ProcessingModeType::createLax();
+        $sut2 = ProcessingModeType::createLax();
+        self::assertNotSame($sut1, $sut2);
+    }
+    
+    /**
+     * Tests that createSkip() creates a new instance of ProcessingModeType 
+     * configured in "skip" mode.
+     */
+    public function testCreateSkipReturnsInstanceModeSkip()
+    {
+        $sut = ProcessingModeType::createSkip();
+        self::assertProcessingModeSkip($sut);
+    }
+    
+    /**
+     * Tests that createSkip() creates a new instance.
+     */
+    public function testCreateSkipReturnsNewInstance()
+    {
+        $sut1 = ProcessingModeType::createSkip();
+        $sut2 = ProcessingModeType::createSkip();
+        self::assertNotSame($sut1, $sut2);
+    }
+    
+    /**
+     * Tests that createStrict() creates a new instance of ProcessingModeType 
+     * configured in "strict" mode.
+     */
+    public function testCreateStrictReturnsInstanceModeStrict()
+    {
+        $sut = ProcessingModeType::createStrict();
+        self::assertProcessingModeStrict($sut);
+    }
+    
+    /**
+     * Tests that createStrict() creates a new instance.
+     */
+    public function testCreateStrictReturnsNewInstance()
+    {
+        $sut1 = ProcessingModeType::createStrict();
+        $sut2 = ProcessingModeType::createStrict();
+        self::assertNotSame($sut1, $sut2);
+    }
+}
