a4add32c359311cf7ef5a935230392eb5e9e67db
christophe.maymard <christophe.maymard@hotmail.com>
2018-09-07 13:12:49 +0200

    Create the "nonNegativeInteger" datatype.

---
+++ src/PhpXmlSchema/Datatype/NonNegativeIntegerType.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Datatype;
+
+use PhpXmlSchema\Exception\InvalidValueException;
+
+/**
+ * Represents the "nonNegativeInteger" datatype.
+ * 
+ * It represents non-negative integer numbers (the infinite set {0, 1, 2, ...}).
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class NonNegativeIntegerType
+{
+    /**
+     * The value.
+     * @var \GMP
+     */
+    private $value;
+    
+    /**
+     * Constructor.
+     * 
+     * @param   \GMP    $value  The value to set.
+     */
+    public function __construct(\GMP $value)
+    {
+        $this->setValue($value);
+    }
+    
+    /**
+     * Sets the value.
+     * 
+     * @param   \GMP    $value  The value to set.
+     * 
+     * @throws  InvalidValueException   When the value is invalid.
+     */
+    private function setValue(\GMP $value)
+    {
+        if ($value < 0) {
+            throw new InvalidValueException(\sprintf('"%s" is an invalid non-negative integer.', $value));
+        }
+        
+        $this->value = $value;
+    }
+    
+    /**
+     * Returns the integer representation of the value.
+     * 
+     * @return  \GMP
+     */
+    public function getInteger():\GMP
+    {
+        return $this->value;
+    }
+}
---
+++ test/PhpXmlSchema/Test/Unit/Datatype/NonNegativeIntegerTypeTest.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Datatype;
+
+use PHPUnit\Framework\TestCase;
+use PhpXmlSchema\Datatype\NonNegativeIntegerType;
+use PhpXmlSchema\Exception\InvalidValueException;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Datatype\NonNegativeIntegerType} 
+ * class.
+ * 
+ * @group   type
+ * @group   datatype
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class NonNegativeIntegerTypeTest extends TestCase
+{
+    /**
+     * Tests that __construct() stores the value when it is valid.
+     * 
+     * @param   \GMP    $value  The value to test.
+     * 
+     * @dataProvider    getValidValues
+     */
+    public function test__constructStoresValueWhenItIsValid(\GMP $value)
+    {
+        $sut = new NonNegativeIntegerType($value);
+        self::assertSame($value, $sut->getInteger());
+    }
+    
+    /**
+     * Tests that __construct() throws an exception when the specified value 
+     * is invalid.
+     */
+    public function test__constructThrowsExceptionWhenValueIsInvalid()
+    {
+        $this->expectException(InvalidValueException::class);
+        $this->expectExceptionMessage('"-1" is an invalid non-negative integer.');
+        
+        $sut = new NonNegativeIntegerType(\gmp_init(-1));
+    }
+    
+    /**
+     * Returns a set of valid values.
+     * 
+     * @return  array[]
+     */
+    public function getValidValues():array
+    {
+        $datasets = [];
+        
+        $datasets['Zero.'] = [ \gmp_init(0), ];
+        $datasets['Positive value.'] = [ \gmp_init(3), ];
+        
+        return $datasets;
+    }
+}
