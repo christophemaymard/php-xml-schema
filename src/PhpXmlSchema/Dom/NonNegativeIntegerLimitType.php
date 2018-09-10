98c21a88792c3497551e299a82340508b6002875
christophe.maymard <christophe.maymard@hotmail.com>
2018-09-10 13:23:36 +0200

    Create the non-negative integer limit type.

---
+++ src/PhpXmlSchema/Dom/NonNegativeIntegerLimitType.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+use PhpXmlSchema\Datatype\NonNegativeIntegerType;
+
+/**
+ * Represents the type for a non-negative integer limit.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class NonNegativeIntegerLimitType
+{
+    /**
+     * The limit.
+     * @var NonNegativeIntegerType|NULL
+     */
+    private $limit;
+    
+    /**
+     * Constructor.
+     * 
+     * @param   NonNegativeIntegerType  $limit  The limit to set (optional)(default to NULL).
+     */
+    public function __construct(NonNegativeIntegerType $limit = NULL)
+    {
+        $this->limit = $limit;
+    }
+    
+    /**
+     * Returns the limit.
+     * 
+     * @return  NonNegativeIntegerType|NULL The instance of the limit if it has been set, otherwise NULL.
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+    
+    /**
+     * Indicates whether the limit is unlimited.
+     * 
+     * @return  bool    TRUE if the limit is unlimited, otherwise FALSE.
+     */
+    public function isUnlimited():bool
+    {
+        return $this->limit === NULL;
+    }
+}
---
+++ test/PhpXmlSchema/Test/Unit/Dom/NonNegativeIntegerLimitTypeTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PHPUnit\Framework\TestCase;
+use PhpXmlSchema\Datatype\NonNegativeIntegerType;
+use PhpXmlSchema\Dom\NonNegativeIntegerLimitType;
+use Prophecy\Prophecy\ProphecySubjectInterface;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\NonNegativeIntegerLimitType} 
+ * class.
+ * 
+ * @group   type
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class NonNegativeIntegerLimitTypeTest extends TestCase
+{
+    /**
+     * Tests that __construct() stores an unlimited limit.
+     */
+    public function test_constructStoresUnlimitedWhenNoArgument()
+    {
+        $sut = new  NonNegativeIntegerLimitType();
+        self::assertNull($sut->getLimit());
+        self::assertTrue($sut->isUnlimited());
+    }
+    
+    /**
+     * Tests that __construct() stores a non-negative integer limit.
+     */
+    public function test__constructStoresNonNegativeIntegerLimit()
+    {
+        $nni = $this->createNonNegativeIntegerTypeDummy();
+        $sut = new  NonNegativeIntegerLimitType($nni);
+        self::assertSame($nni, $sut->getLimit());
+        self::assertFalse($sut->isUnlimited());
+    }
+    
+    /**
+     * Creates a dummy for the {@see PhpXmlSchema\Datatype\NonNegativeIntegerType} 
+     * class.
+     * 
+     * @return  ProphecySubjectInterface
+     */
+    private function createNonNegativeIntegerTypeDummy():ProphecySubjectInterface
+    {
+        return $this->prophesize(NonNegativeIntegerType::class)->reveal();
+    }
+}
