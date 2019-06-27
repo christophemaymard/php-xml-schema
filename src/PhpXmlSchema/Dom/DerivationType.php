1da9d3fce32f0f52e66e15cd6d0b7daf14c3dbb8
christophe.maymard <christophe.maymard@hotmail.com>
2019-06-27 19:39:31 +0200

    Create the "derivation" type.

---
+++ src/PhpXmlSchema/Dom/DerivationType.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+/**
+ * Represents the type for a derivation set.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DerivationType
+{
+    /**
+     * @var bool
+     */
+    private $restriction;
+    
+    /**
+     * @var bool
+     */
+    private $extension;
+    
+    /**
+     * @var bool
+     */
+    private $substitution;
+    
+    /**
+     * @var bool
+     */
+    private $list;
+    
+    /**
+     * @var bool
+     */
+    private $union;
+    
+    /**
+     * Constructor.
+     * 
+     * @param   bool    $restriction    The flag for the "restriction" derivation to set.
+     * @param   bool    $extension      The flag for the "extension" derivation to set.
+     * @param   bool    $substitution   The flag for the "substitution" derivation to set.
+     * @param   bool    $list           The flag for the "list" derivation to set.
+     * @param   bool    $union          The flag for the "union" derivation to set.
+     */
+    public function __construct(
+        bool $restriction, 
+        bool $extension, 
+        bool $substitution,
+        bool $list,
+        bool $union
+    ) {
+        $this->restriction = $restriction;
+        $this->extension = $extension;
+        $this->substitution = $substitution;
+        $this->list = $list;
+        $this->union = $union;
+    }
+    
+    /**
+     * Indicates whether one of the derivations is "restriction".
+     * 
+     * @return  bool    TRUE if one of the derivations is "restriction", otherwise FALSE.
+     */
+    public function byRestriction():bool
+    {
+        return $this->restriction;
+    }
+    
+    /**
+     * Indicates whether one of the derivations is "extension".
+     * 
+     * @return  bool    TRUE if one of the derivations is "extension", otherwise FALSE.
+     */
+    public function byExtension():bool
+    {
+        return $this->extension;
+    }
+    
+    /**
+     * Indicates whether one of the derivations is "substitution".
+     * 
+     * @return  bool    TRUE if one of the derivations is "substitution", otherwise FALSE.
+     */
+    public function bySubstitution():bool
+    {
+        return $this->substitution;
+    }
+    
+    /**
+     * Indicates whether one of the derivations is "list".
+     * 
+     * @return  bool    TRUE if one of the derivations is "list", otherwise FALSE.
+     */
+    public function byList():bool
+    {
+        return $this->list;
+    }
+    
+    /**
+     * Indicates whether one of the derivations is "union".
+     * 
+     * @return  bool    TRUE if one of the derivations is "union", otherwise FALSE.
+     */
+    public function byUnion():bool
+    {
+        return $this->union;
+    }
+}
---
+++ test/PhpXmlSchema/Test/Unit/Dom/DerivationTypeTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PHPUnit\Framework\TestCase;
+use PhpXmlSchema\Dom\DerivationType;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\DerivationType} 
+ * class.
+ * 
+ * @group   type
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DerivationTypeTest extends TestCase
+{
+    /**
+     * Tests that byRestriction() returns TRUE, and the others FALSE, when 
+     * TRUE is provided in construct(). 
+     */
+    public function testByRestrictionReturnsTrue()
+    {
+        $sut = new DerivationType(TRUE, FALSE, FALSE, FALSE, FALSE);
+        self::assertTrue($sut->byRestriction());
+        self::assertFalse($sut->byExtension());
+        self::assertFalse($sut->bySubstitution());
+        self::assertFalse($sut->byList());
+        self::assertFalse($sut->byUnion());
+    }
+    
+    /**
+     * Tests that byExtension() returns TRUE, and the others FALSE, when 
+     * TRUE is provided in construct(). 
+     */
+    public function testByExtensionReturnsTrue()
+    {
+        $sut = new DerivationType(FALSE, TRUE, FALSE, FALSE, FALSE);
+        self::assertFalse($sut->byRestriction());
+        self::assertTrue($sut->byExtension());
+        self::assertFalse($sut->bySubstitution());
+        self::assertFalse($sut->byList());
+        self::assertFalse($sut->byUnion());
+    }
+    
+    /**
+     * Tests that bySubstitution() returns TRUE, and the others FALSE, when 
+     * TRUE is provided in construct(). 
+     */
+    public function testBySubstitutionReturnsTrue()
+    {
+        $sut = new DerivationType(FALSE, FALSE, TRUE, FALSE, FALSE);
+        self::assertFalse($sut->byRestriction());
+        self::assertFalse($sut->byExtension());
+        self::assertTrue($sut->bySubstitution());
+        self::assertFalse($sut->byList());
+        self::assertFalse($sut->byUnion());
+    }
+    
+    /**
+     * Tests that byList() returns TRUE, and the others FALSE, when 
+     * TRUE is provided in construct(). 
+     */
+    public function testByListReturnsTrue()
+    {
+        $sut = new DerivationType(FALSE, FALSE, FALSE, TRUE, FALSE);
+        self::assertFalse($sut->byRestriction());
+        self::assertFalse($sut->byExtension());
+        self::assertFalse($sut->bySubstitution());
+        self::assertTrue($sut->byList());
+        self::assertFalse($sut->byUnion());
+    }
+    
+    /**
+     * Tests that byUnion() returns TRUE, and the others FALSE, when 
+     * TRUE is provided in construct(). 
+     */
+    public function testByUnionReturnsTrue()
+    {
+        $sut = new DerivationType(FALSE, FALSE, FALSE, FALSE, TRUE);
+        self::assertFalse($sut->byRestriction());
+        self::assertFalse($sut->byExtension());
+        self::assertFalse($sut->bySubstitution());
+        self::assertFalse($sut->byList());
+        self::assertTrue($sut->byUnion());
+    }
+}
