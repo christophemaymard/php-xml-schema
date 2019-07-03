4633d18d39a1ac4e827bb4fc1dd99a8fc17eeb7a
christophe.maymard <christophe.maymard@hotmail.com>
2019-07-03 14:26:15 +0200

    Create the "namespace" type.

---
+++ src/PhpXmlSchema/Dom/NamespaceListType.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Dom;
+
+use PhpXmlSchema\Datatype\AnyUriType;
+
+/**
+ * Represents the type for a "namespace".
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class NamespaceListType
+{
+    /**
+     * A flag to indicates whether any namespace can be present (default to FALSE).
+     * @var bool
+     */
+    private $any = FALSE;
+    
+    /**
+     * A flag to indicates whether any namespace, that is not the target 
+     * namespace of the parent element, can be present (default to FALSE).
+     * @var bool
+     */
+    private $other = FALSE;
+    
+    /**
+     * A flag to indicates whether the target namespace can be present 
+     * (default to FALSE).
+     * @var bool
+     */
+    private $targetNamespace = FALSE;
+    
+    /**
+     * A flag to indicates whether entities, that are not qualified with a 
+     * namespace, can be present (default to FALSE).
+     * @var bool
+     */
+    private $local = FALSE;
+    
+    /**
+     * An indexed array of URI references of namespaces that can be present 
+     * (default to an empty array).
+     * @var AnyUriType[]
+     */
+    private $namespaces = [];
+    
+    /**
+     * Creates a namespace list where any namespace can be present.
+     * 
+     * @return  NamespaceListType   A new instance of NamespaceListType.
+     */
+    public static function createAny():self
+    {
+        $nsl = new self();
+        $nsl->any = TRUE;
+        
+        return $nsl;
+    }
+    
+    /**
+     * Creates a namespace list where any namespace that is not the target 
+     * namespace of the parent element can be present.
+     * 
+     * @return  NamespaceListType   A new instance of NamespaceListType.
+     */
+    public static function createOther():self
+    {
+        $nsl = new self();
+        $nsl->other = TRUE;
+        
+        return $nsl;
+    }
+    
+    /**
+     * Creates a namespace list where:
+     * - the target namespace can be present or absent,
+     * - entities, that are not qualified with a namespace, can be present or absent,
+     * - a set of URI references of namespaces can be present.
+     * 
+     * @param   bool            $targetNamespace    The flag to indicates whether the target namespace can be present (TRUE).
+     * @param   bool            $local              The flag to indicates whether entities, that are not qualified with a namespace, can be present (TRUE).
+     * @param   AnyUriType[]    $namespaces         An indexed array of URI references of namespaces that can be present (can be empty).
+     * @return  NamespaceListType   A new instance of NamespaceListType.
+     */
+    public static function create(bool $targetNamespace, bool $local, array $namespaces)
+    {
+        $nsl = new self();
+        $nsl->targetNamespace = $targetNamespace;
+        $nsl->local = $local;
+        
+        foreach ($namespaces as $namespace) {
+            $nsl->addNamespace($namespace);
+        }
+        
+        return $nsl;
+    }
+    
+    /**
+     * Indicates whether any namespace can be present.
+     * 
+     * @return  bool    TRUE if any namespace can be present, otherwise FALSE.
+     */
+    public function hasAny():bool
+    {
+        return $this->any;
+    }
+    
+    /**
+     * Indicates whether any namespace, that is not the target namespace of 
+     * the parent element, can be present.
+     * 
+     * @return  bool    TRUE if any namespace that is not the target namespace can be present, otherwise FALSE.
+     */
+    public function hasOther():bool
+    {
+        return $this->other;
+    }
+    
+    /**
+     * Indicates whether the target namespace can be present.
+     * 
+     * @return  bool    TRUE if the target namespace can be present, otherwise FALSE.
+     */
+    public function hasTargetNamespace():bool
+    {
+        return $this->targetNamespace;
+    }
+    
+    /**
+     * Indicates whether entities that are not qualified with a namespace can 
+     * be present.
+     * 
+     * @return  bool    TRUE if entities that are not qualified with a namespace can be present, otherwise FALSE.
+     */
+    public function hasLocal():bool
+    {
+        return $this->local;
+    }
+    
+    /**
+     * Adds the specified URI reference to the list of the namespaces that 
+     * can be present.
+     * 
+     * @param   AnyUriType  $namespace  The URI reference of namespace to add.
+     */
+    private function addNamespace(AnyUriType $namespace)
+    {
+        $this->namespaces[] = $namespace;
+    }
+    
+    /**
+     * Returns all the URI references of namespaces that can be present.
+     * 
+     * @return  AnyUriType[]    An indexed array of AnyUriType instances.
+     */
+    public function getNamespaces():array
+    {
+        return $this->namespaces;
+    }
+}
---
+++ test/PhpXmlSchema/Test/Unit/Dom/NamespaceListTypeTest.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Dom;
+
+use PHPUnit\Framework\TestCase;
+use PhpXmlSchema\Datatype\AnyUriType;
+use PhpXmlSchema\Dom\NamespaceListType;
+use Prophecy\Prophecy\ProphecySubjectInterface;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Dom\NamespaceListType} 
+ * class.
+ * 
+ * @group   type
+ * @group   dom
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class NamespaceListTypeTest extends TestCase
+{
+    /**
+     * Tests that __construct() creates an instance of NamespaceListType where:
+     * - any namespace is not present,
+     * - any namespace, that is not the target namespace of the parent 
+     * element, is not present,
+     * - the target namespace is not present,
+     * - entities, that are not qualified with a namespace, are not present,
+     * - there is no URI references of namespaces.
+     */
+    public function test__construct()
+    {
+        $sut = new NamespaceListType();
+        self::assertFalse($sut->hasAny());
+        self::assertFalse($sut->hasOther());
+        self::assertFalse($sut->hasTargetNamespace());
+        self::assertFalse($sut->hasLocal());
+        self::assertSame([], $sut->getNamespaces());
+    }
+    
+    /**
+     * Tests that createAny() creates a new instance of NamespaceListType 
+     * where any namespace is present.
+     */
+    public function testCreateAnyReturnsNewInstanceWithAnyNamespace()
+    {
+        $sut = NamespaceListType::createAny();
+        $nsl2 = NamespaceListType::createAny();
+        self::assertNotSame($sut, $nsl2);
+        self::assertTrue($sut->hasAny());
+        self::assertFalse($sut->hasOther());
+        self::assertFalse($sut->hasTargetNamespace());
+        self::assertFalse($sut->hasLocal());
+        self::assertSame([], $sut->getNamespaces());
+    }
+    
+    /**
+     * Tests that createOther() creates a new instance of NamespaceListType 
+     * where any namespace, that is not the target namespace of the parent 
+     * element, is present.
+     */
+    public function testCreateOtherReturnsNewInstanceWithOtherNamespace()
+    {
+        $sut = NamespaceListType::createOther();
+        $nsl2 = NamespaceListType::createOther();
+        self::assertNotSame($sut, $nsl2);
+        self::assertFalse($sut->hasAny());
+        self::assertTrue($sut->hasOther());
+        self::assertFalse($sut->hasTargetNamespace());
+        self::assertFalse($sut->hasLocal());
+        self::assertSame([], $sut->getNamespaces());
+    }
+    
+    /**
+     * Tests that create() creates a new instance of NamespaceListType where:
+     * - the target namespace is not present,
+     * - entities, that are not qualified with a namespace, are not present,
+     * - there is no URI references of namespaces.
+     */
+    public function testCreateReturnsNewInstance()
+    {
+        $sut = NamespaceListType::create(FALSE, FALSE, []);
+        $nsl2 = NamespaceListType::create(FALSE, FALSE, []);
+        self::assertNotSame($sut, $nsl2);
+        self::assertFalse($sut->hasAny());
+        self::assertFalse($sut->hasOther());
+        self::assertFalse($sut->hasTargetNamespace());
+        self::assertFalse($sut->hasLocal());
+        self::assertSame([], $sut->getNamespaces());
+    }
+    
+    /**
+     * Tests that create() creates a new instance of NamespaceListType where:
+     * - the target namespace is present,
+     * - entities, that are not qualified with a namespace, are not present,
+     * - there is no URI references of namespaces.
+     */
+    public function testCreateReturnsNewInstanceWithTargetNamespace()
+    {
+        $sut = NamespaceListType::create(TRUE, FALSE, []);
+        $nsl2 = NamespaceListType::create(TRUE, FALSE, []);
+        self::assertNotSame($sut, $nsl2);
+        self::assertFalse($sut->hasAny());
+        self::assertFalse($sut->hasOther());
+        self::assertTrue($sut->hasTargetNamespace());
+        self::assertFalse($sut->hasLocal());
+        self::assertSame([], $sut->getNamespaces());
+    }
+    
+    /**
+     * Tests that create() creates a new instance of NamespaceListType where:
+     * - the target namespace is not present,
+     * - entities, that are not qualified with a namespace, are present,
+     * - there is no URI references of namespaces.
+     */
+    public function testCreateReturnsNewInstanceWithLocalNamespace()
+    {
+        $sut = NamespaceListType::create(FALSE, TRUE, []);
+        $nsl2 = NamespaceListType::create(FALSE, TRUE, []);
+        self::assertNotSame($sut, $nsl2);
+        self::assertFalse($sut->hasAny());
+        self::assertFalse($sut->hasOther());
+        self::assertFalse($sut->hasTargetNamespace());
+        self::assertTrue($sut->hasLocal());
+        self::assertSame([], $sut->getNamespaces());
+    }
+    
+    /**
+     * Tests that create() creates a new instance of NamespaceListType where:
+     * - the target namespace is not present,
+     * - entities, that are not qualified with a namespace, are not present,
+     * - there is a set of URI references of namespaces.
+     */
+    public function testCreateReturnsNewInstanceWithNamespaces()
+    {
+        $namespaces = [];
+        $namespaces[] = $this->createAnyUriTypeDummy();
+        $namespaces[] = $this->createAnyUriTypeDummy();
+        $namespaces[] = $this->createAnyUriTypeDummy();
+        
+        $sut = NamespaceListType::create(FALSE, FALSE, $namespaces);
+        $nsl2 = NamespaceListType::create(FALSE, FALSE, $namespaces);
+        self::assertNotSame($sut, $nsl2);
+        self::assertFalse($sut->hasAny());
+        self::assertFalse($sut->hasOther());
+        self::assertFalse($sut->hasTargetNamespace());
+        self::assertFalse($sut->hasLocal());
+        self::assertSame($namespaces, $sut->getNamespaces());
+    }
+    
+    /**
+     * Creates a dummy for the {@see PhpXmlSchema\Datatype\AnyUriType} class.
+     * 
+     * @return  ProphecySubjectInterface
+     */
+    private function createAnyUriTypeDummy():ProphecySubjectInterface
+    {
+        return $this->prophesize(AnyUriType::class)->reveal();
+    }
+}
