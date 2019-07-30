e9ea1e6f26fc6b96718eec8836354adb3d74b430
christophe.maymard <christophe.maymard@hotmail.com>
2019-07-30 10:47:49 +0200

    Create the DFA class.
    - Introduced the "fa" and "validation" test groups.

---
+++ src/PhpXmlSchema/Validation/DFA.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018-2019, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Validation;
+
+/**
+ * Represents a Deterministic Finite Automaton (DFA).
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DFA
+{
+    /**
+     * The current state.
+     * NULL means that the current state has been invalidated.
+     * @var int|NULL
+     */
+    private $currentState;
+    
+    /**
+     * The state transition table.
+     * @var array[]
+     */
+    private $transitions = [];
+    
+    /**
+     * The final states.
+     * @var int[]
+     */
+    private $finalStates = [];
+    
+    /**
+     * Constructor.
+     * 
+     * @param   int $initialState   The state to initialize the current state with.
+     */
+    public function __construct(int $initialState)
+    {
+        $this->currentState = $initialState;
+    }
+    
+    /**
+     * Defines a transition that associates a state and a symbol with a next 
+     * state.
+     * 
+     * If a transition is already defined with the specified state and symbol 
+     * then the next state will be updated with the new one.
+     * 
+     * @param   int $state      The state of the transition.
+     * @param   int $symbol     The symbol of the transition.
+     * @param   int $nextState  The next state to associate with.
+     */
+    public function addTransition(int $state, int $symbol, int $nextState)
+    {
+        $this->transitions[$state][$symbol] = $nextState;
+    }
+    
+    /**
+     * Adds the specified state to the final state set.
+     * 
+     * @param   int $state  The state to add.
+     */
+    public function addFinalState(int $state)
+    {
+        $this->finalStates[] = $state;
+    }
+    
+    /**
+     * Finds a transition with the current state and the specified symbol.
+     * 
+     * If a transition is defined then the current state is updated with the 
+     * associated next state, otherwise the current is invalidated (i.e. set 
+     * to NULL).
+     * 
+     * @param   int $symbol The symbol to add.
+     */
+    public function addSymbol(int $symbol)
+    {
+        $this->currentState = ($this->acceptSymbol($symbol)) ? 
+            $this->transitions[$this->currentState][$symbol] : 
+            NULL;
+    }
+    
+    /**
+     * Indicates whether a transition is defined with the current state and 
+     * the specified symbol.
+     * 
+     * @param   int $symbol The symbol to check.
+     * @return  bool    TRUE if the symbol is accpeted in the current state, otherwis FALSE.
+     */
+    public function acceptSymbol(int $symbol):bool
+    {
+        return isset($this->transitions[$this->currentState][$symbol]);
+    }
+    
+    /**
+     * Returns all the symbols that are accepted in the current state.
+     * 
+     * If the current state is NULL or there is no accepted symbol in the 
+     * current state then an empty array is returned.
+     * 
+     * @return  int[]   An indexed array of accpeted symbols.
+     */
+    public function getAcceptedSymbols():array
+    {
+        return (isset($this->transitions[$this->currentState])) ? 
+            \array_keys($this->transitions[$this->currentState]) : 
+            [];
+    }
+    
+    /**
+     * Returns the value of the current state.
+     * 
+     * @return  int|NULL    The value of the current state, otherwise NULL if it has been invalidated.
+     */
+    public function getCurrentState()
+    {
+        return $this->currentState;
+    }
+    
+    /**
+     * Indicates whether the current is a final state.
+     * 
+     * @return  bool    TRUE if the current state is a final state, otherwise FALSE.
+     */
+    public function isValid():bool
+    {
+        return \in_array($this->currentState, $this->finalStates, TRUE);
+    }
+}
---
+++ test/PhpXmlSchema/Test/Unit/Validation/DFATest.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018-2019, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema\Test\Unit\Validation;
+
+use PHPUnit\Framework\TestCase;
+use PhpXmlSchema\Validation\DFA;
+
+/**
+ * Represents the unit tests for the {@see PhpXmlSchema\Validation\DFA} class.
+ * 
+ * @group   fa
+ * @group   validation
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DFATest extends TestCase
+{
+    /**
+     * Tests that __construct() initializes the current state with the 
+     * initial state.
+     */
+    public function test__constructInitializeCurrentStateWithInitialState()
+    {
+        $sut = new DFA(-1);
+        self::assertSame(-1, $sut->getCurrentState());
+    }
+    
+    /**
+     * Tests that getCurrentState() returns NULL when adding a symbol whereas 
+     * there is no defined transition.
+     */
+    public function testGetCurrentStateReturnsNullWhenAddingSymbolWithNoTransition()
+    {
+        $sut = new DFA(-1);
+        $sut->addSymbol(0);
+        self::assertNull($sut->getCurrentState(), 'No transition.');
+    }
+    
+    /**
+     * Tests that getCurrentState() returns the next state of the transition 
+     * that matches the current state and the adding symbol.
+     */
+    public function testGetCurrentStateReturnsIntWhenAddingSymbolMatchesTransitionStateSymbol()
+    {
+        $sut = new DFA(-1);
+        $sut->addTransition(-1, 100, 1);
+        $sut->addSymbol(100);
+        self::assertSame(1, $sut->getCurrentState());
+    }
+    
+    /**
+     * Tests that addTransition() updates the next state of the transition 
+     * that matches the state and the symbol.
+     */
+    public function testAddTransitionUpdatesTransitionNextStateWhenAddingTransitionWithSameStateSameSymbolAndOtherNextState()
+    {
+        $sut = new DFA(0);
+        $sut->addTransition(0, 100, 1);
+        $sut->addTransition(0, 100, 2);
+        $sut->addSymbol(100);
+        self::assertSame(2, $sut->getCurrentState());
+    }
+    
+    /**
+     * Tests that getCurrentState() returns NULL when:
+     * - the current state is valid and there is no matching transition, or
+     * - the current state is invalidated
+     */
+    public function testGetCurrentStateReturnsNullWhenAddingSymbolDoesNotMatchTransition()
+    {
+        $sut = new DFA(0);
+        $sut->addTransition(0, 100, 1);
+        $sut->addSymbol(-1);
+        self::assertNull($sut->getCurrentState(), 'No matching transition.');
+        $sut->addSymbol(100);
+        self::assertNull($sut->getCurrentState(), 'The current state is invalidated.');
+    }
+    
+    /**
+     * Tests that acceptSymbol() returns TRUE when there is, at least, 
+     * 1 transition defined with the current state.
+     */
+    public function testAcceptSymbolReturnsTrue()
+    {
+        $sut = new DFA(0);
+        $sut->addTransition(0, 100, 1);
+        self::assertTrue($sut->acceptSymbol(100), '1 transition.');
+        $sut->addSymbol(100);
+        $sut->addTransition(1, 1000, 2);
+        $sut->addTransition(1, 2000, 2);
+        $sut->addTransition(1, 3000, 0);
+        self::assertTrue($sut->acceptSymbol(3000), '3 transitions.');
+    }
+    
+    /**
+     * Tests that acceptSymbol() returns FALSE when:
+     * - there is no transition, or
+     * - there is a transition with the current state but not the symbol, or
+     * - the current state is invalidated
+     */
+    public function testAcceptSymbolReturnsFalse()
+    {
+        $sut = new DFA(0);
+        self::assertFalse($sut->acceptSymbol(200), 'No transition.');
+        $sut->addTransition(0, 100, 1);
+        self::assertFalse($sut->acceptSymbol(200), 'Transition with the current state but not the symbol.');
+        $sut->addSymbol(-1);
+        self::assertFalse($sut->acceptSymbol(200), 'The current state is invalidated.');
+    }
+    
+    /**
+     * Tests that getAcceptedSymbols() returns an empty array when:
+     * - there is no defined transition with the current state, or
+     * - the current state is invalidated
+     */
+    public function testGetAcceptedSymbolsReturnsEmptyArray()
+    {
+        $sut = new DFA(0);
+        self::assertSame([], $sut->getAcceptedSymbols(), 'No transition.');
+        $sut->addSymbol(-1);
+        self::assertSame([], $sut->getAcceptedSymbols(), 'The current state is invalidated.');
+    }
+    
+    /**
+     * Tests that getAcceptedSymbols() returns an indexed array of integers 
+     * when there is, at least, 1 transition defined with the current state.
+     */
+    public function testGetAcceptedSymbolsReturnsIndexedArrayIntegers()
+    {
+        $sut = new DFA(0);
+        $sut->addTransition(0, 10, 1);
+        $sut->addTransition(0, 20, 2);
+        $sut->addTransition(0, 30, 3);
+        self::assertSame([ 10, 20, 30, ], $sut->getAcceptedSymbols(), '3 transitions.');
+        $sut->addSymbol(10);
+        $sut->addTransition(1, 400, 4);
+        self::assertSame([ 400, ], $sut->getAcceptedSymbols(), '1 transition.');
+    }
+    
+    /**
+     * Tests that isValid() returns FALSE when:
+     * - the final state set is empty, or
+     * - the current state is not part of the final state set, or
+     * - the current state is invalidated
+     */
+    public function testIsValidReturnsFalseWhenCurrentStateDoesNotMatchFinalState()
+    {
+        $sut = new DFA(0);
+        self::assertFalse($sut->isValid(), 'Final state set empty.');
+        $sut->addFinalState(1);
+        $sut->addFinalState(2);
+        $sut->addFinalState(3);
+        self::assertFalse($sut->isValid(), 'The current state is not part of the final state set.');
+        $sut->addSymbol(100);
+        self::assertFalse($sut->isValid(), 'The current state is invalidated.');
+    }
+    
+    /**
+     * Tests that isValid() returns TRUE when the current state is part of 
+     * the final state set.
+     */
+    public function testIsValidReturnsTrueWhenCurrentStateMatchesFinalState()
+    {
+        $sut = new DFA(0);
+        $sut->addFinalState(0);
+        self::assertTrue($sut->isValid());
+        $sut->addTransition(0, 1, 10);
+        $sut->addFinalState(10);
+        $sut->addSymbol(1);
+        self::assertTrue($sut->isValid());
+    }
+}
