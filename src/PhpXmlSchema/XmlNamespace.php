9e8b5c47e1b35515d6166883ecb0bef597d06a5d
christophe.maymard <christophe.maymard@hotmail.com>
2019-07-30 14:26:17 +0200

    Create the XmlNamespace class.

---
+++ src/PhpXmlSchema/XmlNamespace.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the PhpXmlSchema library.
+ * 
+ * @copyright   2018-2019, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpXmlSchema;
+
+/**
+ * Represents all the values related to XML namespaces.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class XmlNamespace
+{
+    /**
+     * The XML Schema 1.0 namespace.
+     */
+    const XML_SCHEMA_1_0 = 'http://www.w3.org/2001/XMLSchema';
+}
