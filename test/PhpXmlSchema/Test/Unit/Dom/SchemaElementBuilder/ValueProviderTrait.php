ca391ccd0b65e1d068858e7ebb5e40404fde701d
christophe.maymard <christophe.maymard@hotmail.com>
2019-08-25 12:54:31 +0200

    Add datasets for "boolean" datatype.

--- test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/ValueProviderTrait.php
+++ test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/ValueProviderTrait.php
@@ -690,4 +690,67 @@ trait ValueProviderTrait
             ], 
         ];
     }
+    
+    /**
+     * Returns a set of valid "boolean" datatype values.
+     * 
+     * @return  array[]
+     */
+    public function getValidBooleanValues():array
+    {
+        return [
+            'true (string)' => [ 
+                'true', 
+                TRUE, 
+            ], 
+            'true (numeric)' => [ 
+                '1', 
+                TRUE, 
+            ], 
+            'true (string) surrounded by white spaces' => [ 
+                " \t \r  \n  true   \t \r  \n   ", 
+                TRUE, 
+            ], 
+            'true (numeric) surrounded by white spaces' => [ 
+                " \n  \t \r  1   \r \t   \n  ", 
+                TRUE, 
+            ], 
+            'false (string)' => [ 
+                'false', 
+                FALSE, 
+            ], 
+            'false (numeric)' => [ 
+                '0', 
+                FALSE, 
+            ], 
+            'false (string) surrounded by white spaces' => [ 
+                "  \n  \r  \t false   \r ", 
+                FALSE, 
+            ], 
+            'false (numeric) surrounded by white spaces' => [ 
+                "   \r  \r 0   \r  \t   \n ", 
+                FALSE, 
+            ], 
+        ];
+    }
+    
+    /**
+     * Returns a set of invalid "boolean" datatype values.
+     * 
+     * @return  array[]
+     */
+    public function getInvalidBooleanValues():array
+    {
+        return [
+            'true (uppercase)' => [ 
+                'True', 
+            ], 
+            'false (uppercase)' => [ 
+                'False', 
+            ], 
+            'Numeric other than 0 or 1' => [ 
+                '2', 
+            ], 
+        ];
+    }
 }
