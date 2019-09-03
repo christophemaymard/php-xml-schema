070d3b10fbaeaa419df2d4819691d9650a10502d
christophe.maymard <christophe.maymard@hotmail.com>
2019-09-03 09:10:22 +0200

    Builder creates "enumeration" element in "restriction" element (simpleRestrictionType).
    - Updated SchemaElementBuilder::buildEnumerationElement() to build "enumeration" element in "restriction" element (simpleRestrictionType).
    - Removed BuildEnumerationElementDoesNotCreateElementTestTrait use.
    - Added SimpleContentRestrictionSchemaElementBuilderTest::testBuildEnumerationElementCreateEltWhenSimpleContentRestriction().

--- src/PhpXmlSchema/Dom/SchemaElementBuilder.php
+++ src/PhpXmlSchema/Dom/SchemaElementBuilder.php
@@ -734,10 +734,14 @@ class SchemaElementBuilder implements SchemaBuilderInterface
      */
     public function buildEnumerationElement()
     {
-        if ($this->currentElement instanceof SimpleTypeRestrictionElement) {
-            $elt = new EnumerationElement();
-            $this->currentElement->addEnumerationElement($elt);
-            $this->currentElement = $elt;
+        if ($this->currentElement instanceof ElementInterface) {
+            switch ($this->currentElement->getElementId()) {
+                case ElementId::ELT_SIMPLETYPE_RESTRICTION:
+                case ElementId::ELT_SIMPLECONTENT_RESTRICTION:
+                    $elt = new EnumerationElement();
+                    $this->currentElement->addEnumerationElement($elt);
+                    $this->currentElement = $elt;
+            }
         }
     }
     
--- test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/SimpleContentRestrictionSchemaElementBuilderTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/SimpleContentRestrictionSchemaElementBuilderTest.php
@@ -53,7 +53,6 @@ class SimpleContentRestrictionSchemaElementBuilderTest extends AbstractSchemaEle
     use BuildTypeAttributeDoesNotCreateAttributeTestTrait;
     use BuildRestrictionElementDoesNotCreateElementTestTrait;
     use BuildValueAttributeDoesNotCreateAttributeTestTrait;
-    use BuildEnumerationElementDoesNotCreateElementTestTrait;
     use BuildWhiteSpaceElementDoesNotCreateElementTestTrait;
     use BuildPatternElementDoesNotCreateElementTestTrait;
     use BuildListElementDoesNotCreateElementTestTrait;
@@ -693,5 +692,37 @@ class SimpleContentRestrictionSchemaElementBuilderTest extends AbstractSchemaEle
         self::assertElementNamespaceDeclarations([], $maxls[1]);
         self::assertMaxLengthElementHasNoAttribute($maxls[1]);
         self::assertSame([], $maxls[1]->getElements());
+    }
+    
+    /**
+     * Tests that buildEnumerationElement() creates the element when the 
+     * current element is the "restriction" element (simpleRestrictionType).
+     * 
+     * @group   content
+     * @group   element
+     */
+    public function testBuildEnumerationElementCreateEltWhenSimpleContentRestriction()
+    {
+        $this->sut->buildEnumerationElement();
+        $this->sut->endElement();
+        $this->sut->buildEnumerationElement();
+        $sch = $this->sut->getSchema();
+        
+        self::assertAncestorsNotChanged($sch);
+        
+        $res = self::getCurrentElement($sch);
+        self::assertElementNamespaceDeclarations([], $res);
+        self::assertSimpleContentRestrictionElementHasNoAttribute($res);
+        self::assertCount(2, $res->getElements());
+        
+        $enums = $res->getEnumerationElements();
+        
+        self::assertElementNamespaceDeclarations([], $enums[0]);
+        self::assertEnumerationElementHasNoAttribute($enums[0]);
+        self::assertSame([], $enums[0]->getElements());
+        
+        self::assertElementNamespaceDeclarations([], $enums[1]);
+        self::assertEnumerationElementHasNoAttribute($enums[1]);
+        self::assertSame([], $enums[1]->getElements());
     }
 }
