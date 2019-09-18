f5f0609aee2b13c433debd0690418e5aed2af377
christophe.maymard <christophe.maymard@hotmail.com>
2019-09-18 14:17:04 +0200

    Builder creates "attribute" element (attribute) in "complexType" element (topLevelComplexType).
    - Updated SchemaElementBuilder::buildAttributeElement() to build "attribute" element (attribute) in "complexType" element (topLevelComplexType).
    - Added TopComplexTypeSchemaElementBuilderTest::testBuildAttributeElementCreateEltWhenTopComplexType().
    - Removed BuildAttributeElementDoesNotCreateElementTestTrait use.

--- src/PhpXmlSchema/Dom/SchemaElementBuilder.php
+++ src/PhpXmlSchema/Dom/SchemaElementBuilder.php
@@ -819,17 +819,10 @@ class SchemaElementBuilder implements SchemaBuilderInterface
             switch ($this->currentElement->getElementId()) {
                 case ElementId::ELT_COMPLEXCONTENT_RESTRICTION:
                 case ElementId::ELT_COMPLEXCONTENT_EXTENSION:
+                case ElementId::ELT_COMPLEXTYPE:
                     $elt = new AttributeElement();
                     $this->currentElement->addAttributeElement($elt);
                     $this->currentElement = $elt;
-                    break;
-                case ElementId::ELT_COMPLEXTYPE:
-                    if (!$this->currentElement->getParent() instanceof SchemaElement) {
-                        $elt = new AttributeElement();
-                        $this->currentElement->addAttributeElement($elt);
-                        $this->currentElement = $elt;
-                    }
-                    
                     break;
                 case ElementId::ELT_ATTRIBUTEGROUP:
                     if (!$this->currentElement->getParent() instanceof SchemaElement) {
--- test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/TopComplexTypeSchemaElementBuilderTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/TopComplexTypeSchemaElementBuilderTest.php
@@ -52,7 +52,6 @@ class TopComplexTypeSchemaElementBuilderTest extends AbstractSchemaElementBuilde
     use BuildPublicAttributeDoesNotCreateAttributeTestTrait;
     use BuildSystemNamespaceAttributeDoesNotCreateAttributeTestTrait;
     use BuildDefinitionAnnotationElementDoesNotCreateElementTestTrait;
-    use BuildAttributeElementDoesNotCreateElementTestTrait;
     use BuildDefaultAttributeDoesNotCreateAttributeTestTrait;
     use BuildFixedAttributeDoesNotCreateAttributeTestTrait;
     use BuildTypeAttributeDoesNotCreateAttributeTestTrait;
@@ -637,5 +636,37 @@ class TopComplexTypeSchemaElementBuilderTest extends AbstractSchemaElementBuilde
         self::assertElementNamespaceDeclarations([], $seq);
         self::assertSequenceElementHasNoAttribute($seq);
         self::assertSame([], $seq->getElements());
+    }
+    
+    /**
+     * Tests that buildAttributeElement() creates the element when the 
+     * current element is the "complexType" element (topLevelComplexType).
+     * 
+     * @group   content
+     * @group   element
+     */
+    public function testBuildAttributeElementCreateEltWhenTopComplexType()
+    {
+        $this->sut->buildAttributeElement();
+        $this->sut->endElement();
+        $this->sut->buildAttributeElement();
+        $sch = $this->sut->getSchema();
+        
+        self::assertAncestorsNotChanged($sch);
+        
+        $ct = self::getCurrentElement($sch);
+        self::assertElementNamespaceDeclarations([], $ct);
+        self::assertComplexTypeElementHasNoAttribute($ct);
+        self::assertCount(2, $ct->getElements());
+        
+        $attrs = $ct->getAttributeElements();
+        
+        self::assertElementNamespaceDeclarations([], $attrs[0]);
+        self::assertAttributeElementHasNoAttribute($attrs[0]);
+        self::assertSame([], $attrs[0]->getElements());
+        
+        self::assertElementNamespaceDeclarations([], $attrs[1]);
+        self::assertAttributeElementHasNoAttribute($attrs[1]);
+        self::assertSame([], $attrs[1]->getElements());
     }
 }
