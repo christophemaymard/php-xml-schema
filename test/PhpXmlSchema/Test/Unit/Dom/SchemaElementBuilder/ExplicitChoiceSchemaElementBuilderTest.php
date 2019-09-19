3b9eb339dbfffd589b033201e25d482163170646
christophe.maymard <christophe.maymard@hotmail.com>
2019-09-20 00:41:11 +0200

    Factorize schema builder tests related to "choice" element.

--- test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/ExplicitChoiceSchemaElementBuilderTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/ExplicitChoiceSchemaElementBuilderTest.php
@@ -10,7 +10,6 @@ namespace PhpXmlSchema\Test\Unit\Dom\SchemaElementBuilder;
 use PhpXmlSchema\Dom\SchemaElement;
 use PhpXmlSchema\Dom\SchemaElementBuilder;
 use PhpXmlSchema\Exception\InvalidValueException;
-use PhpXmlSchema\Test\Unit\Datatype\NCNameTypeProviderTrait;
 use PhpXmlSchema\Test\Unit\Datatype\NonNegativeIntegerTypeProviderTrait;
 use PhpXmlSchema\Test\Unit\Dom\NonNegativeIntegerLimitTypeProviderTrait;
 
@@ -23,95 +22,10 @@ use PhpXmlSchema\Test\Unit\Dom\NonNegativeIntegerLimitTypeProviderTrait;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class ExplicitChoiceSchemaElementBuilderTest extends AbstractSchemaElementBuilderTestCase
+class ExplicitChoiceSchemaElementBuilderTest extends AbstractChoiceSchemaElementBuilderTestCase
 {
-    use NCNameTypeProviderTrait;
     use NonNegativeIntegerLimitTypeProviderTrait;
     use NonNegativeIntegerTypeProviderTrait;
-    
-    use BindNamespaceTestTrait;
-    
-    use BuildAttributeFormDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildBlockDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildElementFormDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildFinalDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildTargetNamespaceAttributeDoesNotCreateAttributeTestTrait;
-    use BuildVersionAttributeDoesNotCreateAttributeTestTrait;
-    use BuildLangAttributeDoesNotCreateAttributeTestTrait;
-    use BuildCompositionAnnotationElementDoesNotCreateElementTestTrait;
-    use BuildAppInfoElementDoesNotCreateElementTestTrait;
-    use BuildSourceAttributeDoesNotCreateAttributeTestTrait;
-    use BuildLeafElementContentDoesNotCreateContentTestTrait;
-    use BuildDocumentationElementDoesNotCreateElementTestTrait;
-    use BuildImportElementDoesNotCreateElementTestTrait;
-    use BuildNamespaceAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSchemaLocationAttributeDoesNotCreateAttributeTestTrait;
-    use BuildIncludeElementDoesNotCreateElementTestTrait;
-    use BuildNotationElementDoesNotCreateElementTestTrait;
-    use BuildNameAttributeDoesNotCreateAttributeTestTrait;
-    use BuildPublicAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSystemNamespaceAttributeDoesNotCreateAttributeTestTrait;
-    use BuildDefinitionAnnotationElementDoesNotCreateElementTestTrait;
-    use BuildAttributeElementDoesNotCreateElementTestTrait;
-    use BuildDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildFixedAttributeDoesNotCreateAttributeTestTrait;
-    use BuildTypeAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSimpleTypeElementDoesNotCreateElementTestTrait;
-    use BuildRestrictionElementDoesNotCreateElementTestTrait;
-    use BuildBaseAttributeDoesNotCreateAttributeTestTrait;
-    use BuildMinExclusiveElementDoesNotCreateElementTestTrait;
-    use BuildValueAttributeDoesNotCreateAttributeTestTrait;
-    use BuildMinInclusiveElementDoesNotCreateElementTestTrait;
-    use BuildMaxExclusiveElementDoesNotCreateElementTestTrait;
-    use BuildMaxInclusiveElementDoesNotCreateElementTestTrait;
-    use BuildTotalDigitsElementDoesNotCreateElementTestTrait;
-    use BuildFractionDigitsElementDoesNotCreateElementTestTrait;
-    use BuildLengthElementDoesNotCreateElementTestTrait;
-    use BuildMinLengthElementDoesNotCreateElementTestTrait;
-    use BuildMaxLengthElementDoesNotCreateElementTestTrait;
-    use BuildEnumerationElementDoesNotCreateElementTestTrait;
-    use BuildWhiteSpaceElementDoesNotCreateElementTestTrait;
-    use BuildPatternElementDoesNotCreateElementTestTrait;
-    use BuildListElementDoesNotCreateElementTestTrait;
-    use BuildItemTypeAttributeDoesNotCreateAttributeTestTrait;
-    use BuildUnionElementDoesNotCreateElementTestTrait;
-    use BuildMemberTypesAttributeDoesNotCreateAttributeTestTrait;
-    use BuildFinalAttributeDoesNotCreateAttributeTestTrait;
-    use BuildAttributeGroupElementDoesNotCreateElementTestTrait;
-    use BuildFormAttributeDoesNotCreateAttributeTestTrait;
-    use BuildRefAttributeDoesNotCreateAttributeTestTrait;
-    use BuildUseAttributeDoesNotCreateAttributeTestTrait;
-    use BuildAnyAttributeElementDoesNotCreateElementTestTrait;
-    use BuildProcessContentsAttributeDoesNotCreateAttributeTestTrait;
-    use BuildComplexTypeElementDoesNotCreateElementTestTrait;
-    use BuildAbstractAttributeDoesNotCreateAttributeTestTrait;
-    use BuildBlockAttributeDoesNotCreateAttributeTestTrait;
-    use BuildMixedAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSimpleContentElementDoesNotCreateElementTestTrait;
-    use BuildExtensionElementDoesNotCreateElementTestTrait;
-    use BuildComplexContentElementDoesNotCreateElementTestTrait;
-    use BuildAllElementDoesNotCreateElementTestTrait;
-    use BuildNillableAttributeDoesNotCreateAttributeTestTrait;
-    use BuildUniqueElementDoesNotCreateElementTestTrait;
-    use BuildSelectorElementDoesNotCreateElementTestTrait;
-    use BuildFieldElementDoesNotCreateElementTestTrait;
-    use BuildXPathAttributeDoesNotCreateAttributeTestTrait;
-    use BuildKeyElementDoesNotCreateElementTestTrait;
-    use BuildKeyRefElementDoesNotCreateElementTestTrait;
-    use BuildReferAttributeDoesNotCreateAttributeTestTrait;
-    
-    /**
-     * {@inheritDoc}
-     */
-    public static function assertSchemaElementNotChanged(SchemaElement $sch)
-    {
-        self::assertAncestorsNotChanged($sch);
-        
-        $choice = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $choice);
-        self::assertChoiceElementHasNoAttribute($choice);
-        self::assertSame([], $choice->getElements());
-    }
     
     /**
      * {@inheritDoc}
@@ -157,14 +71,6 @@ class ExplicitChoiceSchemaElementBuilderTest extends AbstractSchemaElementBuilde
     /**
      * {@inheritDoc}
      */
-    public static function assertCurrentElementHasNotAttribute(SchemaElement $sch)
-    {
-        self::assertChoiceElementHasNoAttribute(self::getCurrentElement($sch));
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
     protected static function getCurrentElement(SchemaElement $sch)
     {
         return $sch->getComplexTypeElements()[0]
@@ -197,58 +103,6 @@ class ExplicitChoiceSchemaElementBuilderTest extends AbstractSchemaElementBuilde
     protected function tearDown()
     {
         $this->sut = NULL;
-    }
-    
-    /**
-     * Tests that buildIdAttribute() creates the attribute when the current 
-     * element is the "choice" element (explicitGroup) and the value is valid.
-     * 
-     * @param   string  $value  The value to test.
-     * @param   string  $id     The expected value for the ID.
-     * 
-     * @group           attribute
-     * @group           parsing
-     * @dataProvider    getValidNCNameTypeWSValues
-     */
-    public function testBuildIdAttributeCreatesAttrWhenExplicitChoiceAndValueIsValid(
-        string $value, 
-        string $id
-    ) {
-        $this->sut->buildIdAttribute($value);
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $choice = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $choice);
-        self::assertChoiceElementHasOnlyIdAttribute($choice);
-        self::assertSame($id, $choice->getId()->getId());
-        self::assertSame([], $choice->getElements());
-    }
-    
-    /**
-     * Tests that buildIdAttribute() throws an exception when the current 
-     * element is the "choice" element (explicitGroup) and the value is 
-     * invalid.
-     * 
-     * @param   string  $value  The value to test.
-     * @param   string  $mValue The string representation of the value in the exception message.
-     * 
-     * @group           attribute
-     * @group           parsing
-     * @dataProvider    getInvalidNCNameTypeWSValues
-     */
-    public function testBuildIdAttributeThrowsExceptionWhenExplicitChoiceAndValueIsInvalid(
-        string $value, 
-        string $mValue
-    ) {
-        $this->expectException(InvalidValueException::class);
-        $this->expectExceptionMessage(\sprintf(
-            '"%s" is an invalid ID datatype.', 
-            $mValue
-        ));
-        
-        $this->sut->buildIdAttribute($value);
     }
     
     /**
@@ -374,190 +228,5 @@ class ExplicitChoiceSchemaElementBuilderTest extends AbstractSchemaElementBuilde
         ));
         
         $this->sut->buildMinOccursAttribute($value);
-    }
-    
-    /**
-     * Tests that buildAnnotationElement() creates the element when the 
-     * current element is the "choice" element (explicitGroup).
-     * 
-     * @group   content
-     * @group   element
-     */
-    public function testBuildAnnotationElementCreateEltWhenExplicitChoice()
-    {
-        $this->sut->buildAnnotationElement();
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $choice = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $choice);
-        self::assertChoiceElementHasNoAttribute($choice);
-        self::assertCount(1, $choice->getElements());
-        
-        $ann = $choice->getAnnotationElement();
-        self::assertElementNamespaceDeclarations([], $ann);
-        self::assertAnnotationElementHasNoAttribute($ann);
-        self::assertSame([], $ann->getElements());
-    }
-    
-    /**
-     * Tests that buildElementElement() creates the element when the 
-     * current element is the "choice" element (explicitGroup).
-     * 
-     * @group   content
-     * @group   element
-     */
-    public function testBuildElementElementCreateEltWhenExplicitChoice()
-    {
-        $this->sut->buildElementElement();
-        $this->sut->endElement();
-        $this->sut->buildElementElement();
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $choice = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $choice);
-        self::assertChoiceElementHasNoAttribute($choice);
-        self::assertCount(2, $choice->getElements());
-        
-        $elts = $choice->getElementElements();
-        
-        self::assertElementNamespaceDeclarations([], $elts[0]);
-        self::assertElementElementHasNoAttribute($elts[0]);
-        self::assertSame([], $elts[0]->getElements());
-        
-        self::assertElementNamespaceDeclarations([], $elts[1]);
-        self::assertElementElementHasNoAttribute($elts[1]);
-        self::assertSame([], $elts[1]->getElements());
-    }
-    
-    /**
-     * Tests that buildGroupElement() creates the element when the current 
-     * element is the "choice" element (explicitGroup).
-     * 
-     * @group   content
-     * @group   element
-     */
-    public function testBuildGroupElementCreateEltWhenExplicitChoice()
-    {
-        $this->sut->buildGroupElement();
-        $this->sut->endElement();
-        $this->sut->buildGroupElement();
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $choice = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $choice);
-        self::assertChoiceElementHasNoAttribute($choice);
-        self::assertCount(2, $choice->getElements());
-        
-        $grps = $choice->getGroupElements();
-        
-        self::assertElementNamespaceDeclarations([], $grps[0]);
-        self::assertGroupElementHasNoAttribute($grps[0]);
-        self::assertSame([], $grps[0]->getElements());
-        
-        self::assertElementNamespaceDeclarations([], $grps[1]);
-        self::assertGroupElementHasNoAttribute($grps[1]);
-        self::assertSame([], $grps[1]->getElements());
-    }
-    
-    /**
-     * Tests that buildChoiceElement() creates the element when the current 
-     * element is the "choice" element (explicitGroup).
-     * 
-     * @group   content
-     * @group   element
-     */
-    public function testBuildChoiceElementCreateEltWhenExplicitChoice()
-    {
-        $this->sut->buildChoiceElement();
-        $this->sut->endElement();
-        $this->sut->buildChoiceElement();
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $choice = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $choice);
-        self::assertChoiceElementHasNoAttribute($choice);
-        self::assertCount(2, $choice->getElements());
-        
-        $choices = $choice->getChoiceElements();
-        
-        self::assertElementNamespaceDeclarations([], $choices[0]);
-        self::assertChoiceElementHasNoAttribute($choices[0]);
-        self::assertSame([], $choices[0]->getElements());
-        
-        self::assertElementNamespaceDeclarations([], $choices[1]);
-        self::assertChoiceElementHasNoAttribute($choices[1]);
-        self::assertSame([], $choices[1]->getElements());
-    }
-    
-    /**
-     * Tests that buildSequenceElement() creates the element when the current 
-     * element is the "choice" element (explicitGroup).
-     * 
-     * @group   content
-     * @group   element
-     */
-    public function testBuildSequenceElementCreateEltWhenExplicitChoice()
-    {
-        $this->sut->buildSequenceElement();
-        $this->sut->endElement();
-        $this->sut->buildSequenceElement();
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $choice = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $choice);
-        self::assertChoiceElementHasNoAttribute($choice);
-        self::assertCount(2, $choice->getElements());
-        
-        $seqs = $choice->getSequenceElements();
-        
-        self::assertElementNamespaceDeclarations([], $seqs[0]);
-        self::assertSequenceElementHasNoAttribute($seqs[0]);
-        self::assertSame([], $seqs[0]->getElements());
-        
-        self::assertElementNamespaceDeclarations([], $seqs[1]);
-        self::assertSequenceElementHasNoAttribute($seqs[1]);
-        self::assertSame([], $seqs[1]->getElements());
-    }
-    
-    /**
-     * Tests that buildAnyElement() creates the element when the current 
-     * element is the "choice" element (explicitGroup).
-     * 
-     * @group   content
-     * @group   element
-     */
-    public function testBuildAnyElementCreateEltWhenExplicitChoice()
-    {
-        $this->sut->buildAnyElement();
-        $this->sut->endElement();
-        $this->sut->buildAnyElement();
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $choice = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $choice);
-        self::assertChoiceElementHasNoAttribute($choice);
-        self::assertCount(2, $choice->getElements());
-        
-        $anys = $choice->getAnyElements();
-        
-        self::assertElementNamespaceDeclarations([], $anys[0]);
-        self::assertAnyElementHasNoAttribute($anys[0]);
-        self::assertSame([], $anys[0]->getElements());
-        
-        self::assertElementNamespaceDeclarations([], $anys[1]);
-        self::assertAnyElementHasNoAttribute($anys[1]);
-        self::assertSame([], $anys[1]->getElements());
     }
 }
