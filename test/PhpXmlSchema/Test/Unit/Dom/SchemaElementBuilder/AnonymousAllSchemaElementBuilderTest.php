885843bb5caf1fb036a02989d201216c4acf4feb
christophe.maymard <christophe.maymard@hotmail.com>
2019-09-20 01:09:27 +0200

    Factorize schema builder tests related to "all" element.

--- test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/AnonymousAllSchemaElementBuilderTest.php
+++ test/PhpXmlSchema/Test/Unit/Dom/SchemaElementBuilder/AnonymousAllSchemaElementBuilderTest.php
@@ -9,8 +9,6 @@ namespace PhpXmlSchema\Test\Unit\Dom\SchemaElementBuilder;
 
 use PhpXmlSchema\Dom\SchemaElement;
 use PhpXmlSchema\Dom\SchemaElementBuilder;
-use PhpXmlSchema\Exception\InvalidValueException;
-use PhpXmlSchema\Test\Unit\Datatype\NCNameTypeProviderTrait;
 
 /**
  * Represents the unit tests for the {@see PhpXmlSchema\Dom\SchemaElementBuilder} 
@@ -21,99 +19,10 @@ use PhpXmlSchema\Test\Unit\Datatype\NCNameTypeProviderTrait;
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
-class AnonymousAllSchemaElementBuilderTest extends AbstractSchemaElementBuilderTestCase
+class AnonymousAllSchemaElementBuilderTest extends AbstractAllSchemaElementBuilderTestCase
 {
-    use NCNameTypeProviderTrait;
-    
-    use BindNamespaceTestTrait;
-    
-    use BuildAttributeFormDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildBlockDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildElementFormDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildFinalDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildTargetNamespaceAttributeDoesNotCreateAttributeTestTrait;
-    use BuildVersionAttributeDoesNotCreateAttributeTestTrait;
-    use BuildLangAttributeDoesNotCreateAttributeTestTrait;
-    use BuildCompositionAnnotationElementDoesNotCreateElementTestTrait;
-    use BuildAppInfoElementDoesNotCreateElementTestTrait;
-    use BuildSourceAttributeDoesNotCreateAttributeTestTrait;
-    use BuildLeafElementContentDoesNotCreateContentTestTrait;
-    use BuildDocumentationElementDoesNotCreateElementTestTrait;
-    use BuildImportElementDoesNotCreateElementTestTrait;
-    use BuildNamespaceAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSchemaLocationAttributeDoesNotCreateAttributeTestTrait;
-    use BuildIncludeElementDoesNotCreateElementTestTrait;
-    use BuildNotationElementDoesNotCreateElementTestTrait;
-    use BuildNameAttributeDoesNotCreateAttributeTestTrait;
-    use BuildPublicAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSystemNamespaceAttributeDoesNotCreateAttributeTestTrait;
-    use BuildDefinitionAnnotationElementDoesNotCreateElementTestTrait;
-    use BuildAttributeElementDoesNotCreateElementTestTrait;
-    use BuildDefaultAttributeDoesNotCreateAttributeTestTrait;
-    use BuildFixedAttributeDoesNotCreateAttributeTestTrait;
-    use BuildTypeAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSimpleTypeElementDoesNotCreateElementTestTrait;
-    use BuildRestrictionElementDoesNotCreateElementTestTrait;
-    use BuildBaseAttributeDoesNotCreateAttributeTestTrait;
-    use BuildMinExclusiveElementDoesNotCreateElementTestTrait;
-    use BuildValueAttributeDoesNotCreateAttributeTestTrait;
-    use BuildMinInclusiveElementDoesNotCreateElementTestTrait;
-    use BuildMaxExclusiveElementDoesNotCreateElementTestTrait;
-    use BuildMaxInclusiveElementDoesNotCreateElementTestTrait;
-    use BuildTotalDigitsElementDoesNotCreateElementTestTrait;
-    use BuildFractionDigitsElementDoesNotCreateElementTestTrait;
-    use BuildLengthElementDoesNotCreateElementTestTrait;
-    use BuildMinLengthElementDoesNotCreateElementTestTrait;
-    use BuildMaxLengthElementDoesNotCreateElementTestTrait;
-    use BuildEnumerationElementDoesNotCreateElementTestTrait;
-    use BuildWhiteSpaceElementDoesNotCreateElementTestTrait;
-    use BuildPatternElementDoesNotCreateElementTestTrait;
-    use BuildListElementDoesNotCreateElementTestTrait;
-    use BuildItemTypeAttributeDoesNotCreateAttributeTestTrait;
-    use BuildUnionElementDoesNotCreateElementTestTrait;
-    use BuildMemberTypesAttributeDoesNotCreateAttributeTestTrait;
-    use BuildFinalAttributeDoesNotCreateAttributeTestTrait;
-    use BuildAttributeGroupElementDoesNotCreateElementTestTrait;
-    use BuildFormAttributeDoesNotCreateAttributeTestTrait;
-    use BuildRefAttributeDoesNotCreateAttributeTestTrait;
-    use BuildUseAttributeDoesNotCreateAttributeTestTrait;
-    use BuildAnyAttributeElementDoesNotCreateElementTestTrait;
-    use BuildProcessContentsAttributeDoesNotCreateAttributeTestTrait;
-    use BuildComplexTypeElementDoesNotCreateElementTestTrait;
-    use BuildAbstractAttributeDoesNotCreateAttributeTestTrait;
-    use BuildBlockAttributeDoesNotCreateAttributeTestTrait;
-    use BuildMixedAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSimpleContentElementDoesNotCreateElementTestTrait;
-    use BuildExtensionElementDoesNotCreateElementTestTrait;
-    use BuildComplexContentElementDoesNotCreateElementTestTrait;
-    use BuildGroupElementDoesNotCreateElementTestTrait;
     use BuildMaxOccursAttributeDoesNotCreateAttributeTestTrait;
     use BuildMinOccursAttributeDoesNotCreateAttributeTestTrait;
-    use BuildAllElementDoesNotCreateElementTestTrait;
-    use BuildNillableAttributeDoesNotCreateAttributeTestTrait;
-    use BuildChoiceElementDoesNotCreateElementTestTrait;
-    use BuildUniqueElementDoesNotCreateElementTestTrait;
-    use BuildSelectorElementDoesNotCreateElementTestTrait;
-    use BuildFieldElementDoesNotCreateElementTestTrait;
-    use BuildXPathAttributeDoesNotCreateAttributeTestTrait;
-    use BuildKeyElementDoesNotCreateElementTestTrait;
-    use BuildKeyRefElementDoesNotCreateElementTestTrait;
-    use BuildReferAttributeDoesNotCreateAttributeTestTrait;
-    use BuildSequenceElementDoesNotCreateElementTestTrait;
-    use BuildAnyElementDoesNotCreateElementTestTrait;
-    
-    /**
-     * {@inheritDoc}
-     */
-    public static function assertSchemaElementNotChanged(SchemaElement $sch)
-    {
-        self::assertAncestorsNotChanged($sch);
-        
-        $all = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $all);
-        self::assertAllElementHasNoAttribute($all);
-        self::assertSame([], $all->getElements());
-    }
     
     /**
      * {@inheritDoc}
@@ -129,14 +38,6 @@ class AnonymousAllSchemaElementBuilderTest extends AbstractSchemaElementBuilderT
         self::assertGroupElementHasNoAttribute($grp);
         self::assertCount(1, $grp->getElements());
         self::assertNotNull($grp->getModelGroupElement());
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    public static function assertCurrentElementHasNotAttribute(SchemaElement $sch)
-    {
-        self::assertAllElementHasNoAttribute(self::getCurrentElement($sch));
     }
     
     /**
@@ -164,113 +65,5 @@ class AnonymousAllSchemaElementBuilderTest extends AbstractSchemaElementBuilderT
     protected function tearDown()
     {
         $this->sut = NULL;
-    }
-    
-    /**
-     * Tests that buildIdAttribute() creates the attribute when the current 
-     * element is the "all" element (anonymous) and the value is valid.
-     * 
-     * @param   string  $value  The value to test.
-     * @param   string  $id     The expected value for the ID.
-     * 
-     * @group           attribute
-     * @group           parsing
-     * @dataProvider    getValidNCNameTypeWSValues
-     */
-    public function testBuildIdAttributeCreatesAttrWhenAnonymousAllAndValueIsValid(
-        string $value, 
-        string $id
-    ) {
-        $this->sut->buildIdAttribute($value);
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $all = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $all);
-        self::assertAllElementHasOnlyIdAttribute($all);
-        self::assertSame($id, $all->getId()->getId());
-        self::assertSame([], $all->getElements());
-    }
-    
-    /**
-     * Tests that buildIdAttribute() throws an exception when the current 
-     * element is the "all" element (anonymous) and the value is invalid.
-     * 
-     * @param   string  $value  The value to test.
-     * @param   string  $mValue The string representation of the value in the exception message.
-     * 
-     * @group           attribute
-     * @group           parsing
-     * @dataProvider    getInvalidNCNameTypeWSValues
-     */
-    public function testBuildIdAttributeThrowsExceptionWhenAnonymousAllAndValueIsInvalid(
-        string $value, 
-        string $mValue
-    ) {
-        $this->expectException(InvalidValueException::class);
-        $this->expectExceptionMessage(\sprintf(
-            '"%s" is an invalid ID datatype.', 
-            $mValue
-        ));
-        
-        $this->sut->buildIdAttribute($value);
-    }
-    
-    /**
-     * Tests that buildAnnotationElement() creates the element when the 
-     * current element is the "all" element (anonymous).
-     * 
-     * @group   content
-     * @group   element
-     */
-    public function testBuildAnnotationElementCreateEltWhenAnonymousAll()
-    {
-        $this->sut->buildAnnotationElement();
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $all = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $all);
-        self::assertAllElementHasNoAttribute($all);
-        self::assertCount(1, $all->getElements());
-        
-        $ann = $all->getAnnotationElement();
-        self::assertElementNamespaceDeclarations([], $ann);
-        self::assertAnnotationElementHasNoAttribute($ann);
-        self::assertSame([], $ann->getElements());
-    }
-    
-    /**
-     * Tests that buildElementElement() creates the element when the 
-     * current element is the "all" element (anonymous).
-     * 
-     * @group   content
-     * @group   element
-     */
-    public function testBuildElementElementCreateEltWhenAnonymousAll()
-    {
-        $this->sut->buildElementElement();
-        $this->sut->endElement();
-        $this->sut->buildElementElement();
-        $sch = $this->sut->getSchema();
-        
-        self::assertAncestorsNotChanged($sch);
-        
-        $all = self::getCurrentElement($sch);
-        self::assertElementNamespaceDeclarations([], $all);
-        self::assertAllElementHasNoAttribute($all);
-        self::assertCount(2, $all->getElements());
-        
-        $elts = $all->getElementElements();
-        
-        self::assertElementNamespaceDeclarations([], $elts[0]);
-        self::assertElementElementHasNoAttribute($elts[0]);
-        self::assertSame([], $elts[0]->getElements());
-        
-        self::assertElementNamespaceDeclarations([], $elts[1]);
-        self::assertElementElementHasNoAttribute($elts[1]);
-        self::assertSame([], $elts[1]->getElements());
     }
 }
