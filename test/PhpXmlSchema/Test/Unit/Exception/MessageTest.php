6accd01a8aa67ca4fc03c8e9e3f76671c1637581
christophe.maymard <christophe.maymard@hotmail.com>
2020-04-10 14:00:07 +0200

    Update copyright year.

--- test/PhpXmlSchema/Test/Unit/Exception/MessageTest.php
+++ test/PhpXmlSchema/Test/Unit/Exception/MessageTest.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the PhpXmlSchema library.
  * 
- * @copyright   2018-2019, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @copyright   2018-2020, Christophe Maymard <christophe.maymard@hotmail.com>
  * @license     http://opensource.org/licenses/MIT  MIT
  */
 namespace PhpXmlSchema\Test\Unit\Exception;
